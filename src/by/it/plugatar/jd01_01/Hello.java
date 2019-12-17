7f370ab01758696515e10892f429158b0b871e08
henadzi.pluhatar <henadzi.pluhatar@gmail.com>
2019-12-17 12:01:11 +0300

    [+]:Added jd01_01

---
+++ src/by/it/plugatar/jd01_01/Hello.java
@@ -0,0 +1,14 @@
+package by.it.Plugatar.jd01_01;
+
+public class Hello {
+    private String slogan = "HELLO WORLD!";
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;//slogan = slogan;
+    }
+
+    void printSlogan() {
+        System.out.println(slogan);
+    }
+
+}
