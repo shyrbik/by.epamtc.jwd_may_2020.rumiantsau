e980803148ba2f98b62ad9294c25e77df8aabd3c
Andygo298 <andygo298@gmail.com>
2019-12-17 15:56:15 +0300

    TaskC-lesson04 and TaskA-lesson05 complete.

--- src/by/it/lozouski/jd01_04/TaskC.java
+++ src/by/it/lozouski/jd01_04/TaskC.java
@@ -23,68 +23,48 @@ public class TaskC {
     }
 
     public static void mergeSort(double[] array) {
-        mergeSort(array, 0, array.length-1);
+        mergeSort(array, 0, array.length - 1);
     }
 
     private static void mergeSort(double[] array, int left, int right) {
-        if (array == null) {
-            return;
-        }
-        if (array.length < 2) {
-            return;
-        }
-        double[] l = new double[array.length / 2];
-        double[] r = new double[array.length - (array.length / 2)];
-        System.arraycopy(array, 0, l, 0, array.length / 2);
-        System.arraycopy(array, array.length / 2, r, 0, array.length - (array.length / 2));
-        mergeSort(l,0,l.length-1);
-        mergeSort(r,0,r.length-1);
-        array = merge(l, r);
-    }
-
-    private static double[] merge(double[] arrayA, double[] arrayB) {
-        double[] arrayC = new double[arrayA.length + arrayB.length];
-        int positionA = 0, positionB = 0;
-
-        for (int i = 0; i < arrayC.length; i++) {
-            if (positionA == arrayA.length) {
-                arrayC[i] = arrayB[positionB];
-                positionB++;
-            } else if (positionB == arrayB.length) {
-                arrayC[i] = arrayA[positionA];
-                positionA++;
-            } else if (arrayA[positionA] < arrayB[positionB]) {
-                arrayC[i] = arrayA[positionA];
-                positionA++;
-            } else {
-                arrayC[i] = arrayB[positionB];
-                positionB++;
+        if (array == null) return;
+        if (left >= right) return;
+        int l = left;
+        int r = right;
+        double middle = array[(left + (right - left) / 2)];
+        while (l <= r) {
+            while (array[l] < middle) {
+                l++;
+            }
+            while (array[r] > middle) {
+                r--;
+            }
+            if (l <= r) {
+                double tmp = array[l];
+                array[l] = array[r];
+                array[r] = tmp;
+                l++;
+                r--;
             }
         }
-        return arrayC;
+        if (left < r) mergeSort(array, left, r);
+        if (right > l) mergeSort(array, l, right);
     }
-//        double[] resArray = new double[part1.length + part2.length];
-//        double n = part1.length;
-//        double m = part2.length;
-//        int i = 0, j = 0, k = 0;
-//        while (i < n && j < m) {
-//            if (part1[i] <= part2[j]) {
-//                resArray[k] = part1[i];
-//                i++;
-//            } else {
-//                resArray[k] = part2[j];
-//                j++;
-//            }
-//            while (i < n) {
-//                resArray[k] = part1[i];
-//                i++;
-//                k++;
-//            }
-//            while (j < m) {
-//                resArray[k] = part2[j];
-//                j++;
-//                k++;
-//            }
-//        }
-//        return resA
+
+    private static int binarySearch(double[] array, double value) {
+        int leftIndex = 0;
+        int rightIndex = array.length;
+        while (true) {
+            int mid = leftIndex + (rightIndex - leftIndex) / 2;
+
+            if (array[mid] == value)
+                return mid;
+
+            if (array[mid] > value)
+                rightIndex = mid;
+            else
+                leftIndex = mid + 1;
+        }
+    }
 }
+
