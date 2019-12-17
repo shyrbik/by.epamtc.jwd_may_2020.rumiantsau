5f19037a3bff99d2b37e3fd613cc3227716c8632
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-17 19:19:10 +0300

    TaskB2 is done

--- src/by/it/popkov/jd01_05/TaskB.java
+++ src/by/it/popkov/jd01_05/TaskB.java
@@ -5,6 +5,7 @@ import static java.lang.Math.*;
 public class TaskB {
     public static void main(String[] args) {
         step1();
+        step2();
     }
 
     private static void step1() {
@@ -14,6 +15,24 @@ public class TaskB {
                 y += pow(7, a) - cos(x);
             }
             System.out.println("При a=" + a + " Сумма y = " + y);
+        }
+    }
+
+    private static void step2() {
+        for (double x = -5.5; x < 2; x += 0.5) {
+            double b = 0;
+            if (x / 2 < -2 || x / 2 > 0.2) {
+                System.out.println("При x/2=" + x / 2 + " вычисления не определены");
+                continue;
+            } else if (x / 2 > -2 && x / 2 <= -1) {
+                b = sin(pow(x, 2));
+            } else if (x / 2 > -1 && x / 2 < 0.2) {
+                b = cos(pow(x, 2));
+            } else if (x / 2 == 0.2) {
+                b = cos(pow(x, 2)) / sin(pow(x, 2));
+            }
+            double a = log10(abs(b + 2.74));
+            System.out.println("При x/2=" + x / 2 + " a = " + a);
         }
     }
 }
