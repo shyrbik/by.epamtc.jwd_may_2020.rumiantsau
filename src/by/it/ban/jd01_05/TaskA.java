d33f1fc55b689b4a78618463019148616036ccb9
tycoonban <9777687@gmail.com>
2019-12-18 23:56:14 +0300

    Задача 5 okonchena

--- src/by/it/ban/jd01_05/TaskA.java
+++ src/by/it/ban/jd01_05/TaskA.java
@@ -5,7 +5,8 @@ import static java.lang.Math.*;
 public class TaskA {
     public static void main(String[] args) {
         step1();
-       // step2();
+        step2();
+        step3();
     }
 
     private static void step1() {
@@ -16,10 +17,21 @@ public class TaskA {
         System.out.println(z);
     }
 
-//    private static void step2() {
-//        double a=1.21;
-//        double b=0.371;
-//        double y;
-//        y=
-//    }
+    private static void step2() {
+        double a=1.21;
+        double b=0.371;
+        double y;
+        y=tan(pow((a+b),2))-cbrt(a+1.5)+a*pow(b,5)-(b)/log(pow(a,2));
+        System.out.println(y);
+    }
+
+    private static void step3() {
+        double x=12.1;
+        double a;
+        double y;
+        for (a = -5; a <=12 ; a+=3.75) {
+            y=pow(E,a*x)-3.45*a;
+            System.out.println(y);
+        }
+    }
 }
