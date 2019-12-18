7aba98b76fb5e6be5a1881ed63b061cdbe9f1726
ilya <ilyaliberty@gmail.com>
2019-12-18 19:00:38 +0300

    start task 5 step2

--- src/by/it/shpakovskiy/jd01_05/TaskC.java
+++ src/by/it/shpakovskiy/jd01_05/TaskC.java
@@ -5,6 +5,7 @@ import static java.lang.Math.*;
 public class TaskC {
     public static void main(String[] args) {
         step1();
+        System.out.println();
         step2();
     }
 
@@ -20,15 +21,47 @@ public class TaskC {
             if ((i + 1) % 5 == 0) System.out.println();
             x += (9.0 - 5.33) / n;
         }
+        System.out.println();
         double[] b = new double[sizeB];
         for (int i = 0; i < b.length; i++) {
             b[i] = z[z.length - 1 - sizeB + i];
-            System.out.printf("%s[% -3d]= %g", "B", i, b[i]);
+            System.out.printf("%s[% -3d]= %g ", "B", i, b[i]);
             if ((i + 1) % 5 == 0) System.out.println();
         }
+        System.out.println();
     }
 
     private static void step2() {
-
+        int[] a = new int[31];
+        int n = 0;
+        System.out.println("Массив А[индекс по строке]");
+        System.out.println("___________________________________________________________________________");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (round(random() * (450 - 103 + 1) + 103));
+            if (a[i] / 10 > i) n++;
+            System.out.printf("| %s[% -3d]= %d |", "A", i, a[i]);
+            if ((i + 1) % 5 == 0) {
+                System.out.println();
+                System.out.println("|_____________||_____________||_____________||_____________||_____________|");
+            }
+            if (i==a.length-1){
+                int f=0;
+                while (f<4){
+                    System.out.print("|             |");
+                    f++;
+                }
+                System.out.println();
+                System.out.println("|_____________||_____________||_____________||_____________||_____________|");
+            }
+        }
+        System.out.println();
+        int[] b = new int[n];
+        System.out.println("Массив B[индекс по столбцу]");
+        for (int i = 0; i < b.length; i++) {
+            b[i] = a[a.length - 1 - n + i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.printf("| %s[% -3d]= %d |", "B", i, b[i]);
+        }
     }
 }
