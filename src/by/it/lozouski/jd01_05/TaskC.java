7b1c23cf762a024ef139bf30307ac2be42983275
Andygo298 <andygo298@gmail.com>
2019-12-19 09:53:06 +0300

    Task C1-task2 - lesson05 1/2complete.

--- src/by/it/lozouski/jd01_05/TaskC.java
+++ src/by/it/lozouski/jd01_05/TaskC.java
@@ -1,10 +1,14 @@
 package by.it.lozouski.jd01_05;
 
+import java.util.Arrays;
+
 import static java.lang.Math.*;
 
 public class TaskC {
     public static void main(String[] args) {
         stepC1();
+        System.out.println();
+        stepC2();
     }
 
     private static void stepC1() {
@@ -56,5 +60,62 @@ public class TaskC {
             mul++;
         } while (mul < arrayB.length - 1);
         return pow(factB, 1.0 / arrayB.length);
+    }
+
+    private static void stepC2() {
+        int[] arrayA = new int[31];
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayA[i] = (int) round(random() * 347 + 103);
+        }
+        System.out.println(Arrays.toString(arrayA));
+        int sizeB = 0;
+        for (int i = 0; i < arrayA.length; i++) {
+            if (arrayA[i] / 10 > i) sizeB++;
+        }
+        int[] arrayB = new int[sizeB];
+        int x = 0;
+        int y = 0;
+        while (x < arrayA.length) {
+            if (arrayA[x] / 10 > x) {
+                arrayB[y] = arrayA[x];
+                x++;
+                y++;
+            } else x++;
+        }
+        Arrays.sort(arrayB);
+        System.out.println(Arrays.toString(arrayB));
+        System.out.println("Massiv A <index to rows>");
+        tablePrintArrayA(arrayA, "A");
+        System.out.println("Massiv B <index to cols>");
+        tablePrintArrayB(arrayB, "B");
+
+    }
+
+    private static void tablePrintArrayA(int[] arr, String name) {
+        System.out.print("╔═══════════╦═══════════╦═══════════╦═══════════╦═══════════╗\n");
+        for (int i = 1; i < arr.length - 1; i++) {
+
+            System.out.printf("║ %s[%2d]=%d ", name, i, arr[i]);
+            if (i % 5 == 0) {
+                System.out.printf("║%n");
+                System.out.printf("╠═══════════╬═══════════╬═══════════╬═══════════╬═══════════╣%n");
+            }
+        }
+        System.out.print("║           ║");
+        System.out.print("\n╚═══════════╩═══════════╩═══════════╩═══════════╩═══════════╝\n");
+
+    }
+
+    private static void tablePrintArrayB(int[] arr, String name) {
+        System.out.print("╔═══════════╦═══════════╗\n");
+        for (int i = 0; i <=arr.length/2; i++) {
+                System.out.printf("║ %s[%2d]=%d ", name, i, arr[i]);
+            if (!((i+1)+arr.length/2==arr.length)) {
+                System.out.printf("║ %s[%2d]=%d ║%n", name, (i +1) + arr.length / 2, arr[(i + 1) + arr.length / 2]);
+            }else System.out.printf("║           ║%n");
+                if (i < arr.length / 2) System.out.printf("╠═══════════╬═══════════╣%n");
+
+        }
+        System.out.print("╚═══════════╩═══════════╝");
     }
 }
