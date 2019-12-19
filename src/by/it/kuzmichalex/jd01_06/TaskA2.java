bea5413ebac47ad5fe12383d37345efedf72a71a
kuzmichalex <58950628+kuzmichalex@users.noreply.github.com>
2019-12-19 22:59:38 +0300

    Ну это так. Для проверки. Посмотреть, замигают ли лампочки на роутере.

---
+++ src/by/it/kuzmichalex/jd01_06/TaskA2.java
@@ -0,0 +1,46 @@
+package by.it.kuzmichalex.jd01_06;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Тестовое задание A2.
+ * Подсчёт количества вхождений каждого слова в поэме
+ */
+public class TaskA2 {
+    private static String[] lexicon;
+    private static int[] counterWordUsed;
+
+    public static void main(String[] args) {
+        lexicon= new String[0];
+        counterWordUsed= new int[0];
+        Pattern patternRussianWord = Pattern.compile("[А-Яа-яЁё]{1,}");
+        Matcher russianWord = patternRussianWord.matcher(Poem.text);
+        while(russianWord.find()){
+            calcWord(russianWord.group());
+        }
+        for (int i = 0; i < lexicon.length; i++) {
+            System.out.printf("%s=%d%n",lexicon[i],counterWordUsed[i]);
+        }
+    }
+
+    /**
+     * Добавление слова в массив lexicon, если там его ещё нет
+     * и подсчёт количества вхождений этого слова в совпадающем по индексам массиве counterWordUsed
+     * @param wordToCalculate Добавляемое (подсчитываемое слово)
+     * */
+    private static void calcWord(String wordToCalculate) {
+        for (int i = 0; i <lexicon.length ; i++) {
+            if(lexicon[i].equals(wordToCalculate)){
+                counterWordUsed[i]++;
+                return;
+            }
+        }
+        //Слово не найдено. будем добавлять
+        lexicon = Arrays.copyOf(lexicon,lexicon.length+1);
+        lexicon[lexicon.length-1]=wordToCalculate;
+        counterWordUsed=Arrays.copyOf(counterWordUsed,counterWordUsed.length+1);
+        counterWordUsed[counterWordUsed.length-1]=1;
+    }
+}
