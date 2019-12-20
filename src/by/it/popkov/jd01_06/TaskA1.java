17459fd25ab8dde3f8bd6dbd66cec3432c90c702
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-20 12:29:21 +0300

    TaskA1 is done

--- src/by/it/popkov/jd01_06/TaskA1.java
+++ src/by/it/popkov/jd01_06/TaskA1.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class TaskA1 {
     public static void main(String[] args) {
         StringBuilder text = new StringBuilder(Poem.text);
-        Pattern p1 = Pattern.compile("[[А-Я[а-я]]Ёё]{4,}");
+        Pattern p1 = Pattern.compile("[А-Яа-яЁё]{4,}");
         Matcher m1 = p1.matcher(text);
         while (m1.find()){
             text.replace(m1.start()+3, m1.start()+4,"#");
