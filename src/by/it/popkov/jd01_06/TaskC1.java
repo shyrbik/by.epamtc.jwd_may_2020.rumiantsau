b3e824149eb016ca07dc3e3a5d8c679bef119633
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-21 20:56:34 +0300

    TaskC1

--- src/by/it/popkov/jd01_06/TaskC1.java
+++ src/by/it/popkov/jd01_06/TaskC1.java
@@ -20,6 +20,7 @@ public class TaskC1 {
         }
         for (int i = 0; i < rows.length; i++) {
             Pattern p = Pattern.compile(" +");
+            StringBuilder sbRow = new StringBuilder(rows[i]);
 //            String[] word = p.split(rows[i]);
 //            int counter = 0;
 //            for (int j = 0; counter != max; j++) {
@@ -42,15 +43,29 @@ public class TaskC1 {
 //            for (int i1 = 0; i1 < word.length; i1++) {
 //                rows[i] += word[i1];
 //            }
-//            Matcher m = p.matcher(rows[i]);
-//            while (m.find() && rows[i].length() < max) {
-//                StringBuilder sb = new StringBuilder(rows[i]);
-//                sb.insert(m.end()," ");
-//                rows[i] = sb.toString();
-//            }
-            for (int j = 1; rows[i].length() < max; j++) {
-                rows[i] = rows[i].replaceFirst(" ", "  ");
+            int[] pos = new int[0];
+            Matcher m = p.matcher(rows[i]);
+            while (m.find()) {
+                pos = Arrays.copyOf(pos, pos.length + 1);
+                pos[pos.length - 1] = m.start();
             }
+            while (sbRow.length() != max) {
+
+                for (int i1 = pos.length - 1; i1 >= 0; i1--) {
+                    if (sbRow.length() == max) break;
+                    sbRow.insert(pos[i1], " ");
+                }
+                Matcher m2 = p.matcher(sbRow);
+                int newPos = 0;
+                while (m2.find()){
+                    pos[newPos] = m2.start();
+                    newPos++;
+                }
+            }
+            rows[i] = sbRow.toString();
+//            for (int j = 1; rows[i].length() < max; j++) {
+//                rows[i] = rows[i].replaceFirst(" ", "  ");
+//            }
         }
         for (String row : rows) {
             newText.append(row).append("\n");
