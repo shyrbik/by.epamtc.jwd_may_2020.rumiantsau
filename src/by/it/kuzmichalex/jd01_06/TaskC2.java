5670c753e161e30cdb2bf9508e2fc18737d42e68
kuzmichalex <58950628+kuzmichalex@users.noreply.github.com>
2019-12-21 18:12:25 +0300

    Задания A1 A2 B1 B2 С1

---
+++ src/by/it/kuzmichalex/jd01_06/TaskC2.java
@@ -0,0 +1,62 @@
+package by.it.kuzmichalex.jd01_06;
+
+import java.util.Random;
+
+/**
+ * @author kuzmichAlex
+ * Тестовое зедание C2. Формирование случайного длинного текста из слов входной строки
+ * двумя методами и сравнение результата этих методов
+ */
+public class TaskC2 {
+    final static int countOfWords = 10000;
+    final static int initRandomValue = 1234;
+
+    public static void main(String[] args) {
+        final String testText = "Soy un chico delicado Que nacio para el amor Esto coche me ha estopeado Para las pilas senor";
+        long slowTime = System.nanoTime();
+        String resultString = slow(testText);
+        slowTime = System.nanoTime() - slowTime;
+
+        long fastTime = System.nanoTime();
+        resultString = fast(testText);
+        fastTime=fastTime -System.nanoTime();
+    }
+
+    /**
+     * @param inputString Строка текста, слова которого будут применяться для сосотавления случайного текста
+     * @return Случайный текст.
+     * @author kuzmivhalex
+     * Медленная операция составления случайного текста из слов входного предложения
+     */
+    private static String slow(String inputString) {
+        String returnString = new String();
+        String[] arrWords = inputString.split(" {1,}");
+
+        Random pseudoRandom = new Random(initRandomValue);
+
+        for (int i = 0; i < countOfWords; i++) {
+            int randomIdx = pseudoRandom.nextInt(arrWords.length);
+            returnString += arrWords[randomIdx] + " ";
+        }
+        return returnString;
+    }
+
+    /**
+     * @param inputString Строка текста, слова которого будут применяться для сосотавления случайного текста
+     * @return Случайный текст.
+     * @author kuzmivhalex
+     * Быстрая операция составления случайного текста из слов входного предложения
+     */
+    private static String fast(String inputString) {
+        StringBuffer returnString = new StringBuffer();
+        String[] arrWords = inputString.split(" {1,}");
+        Random pseudoRandom = new Random(initRandomValue);
+
+        for (int i = 0; i < countOfWords; i++) {
+            int randomIdx = pseudoRandom.nextInt(arrWords.length);
+            returnString.append(arrWords[randomIdx]);
+            returnString.append(" ");
+        }
+        return returnString.toString();
+    }
+}
