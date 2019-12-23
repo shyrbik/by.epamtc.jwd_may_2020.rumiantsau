285903944ec066aedd33915a589122297cc01937
ApProve1909 <finch2215@gmail.com>
2019-12-23 20:27:49 +0300

    jd04 and jd05 completed. jd04 is doing

--- src/by/it/drozd/jd01_05/TaskB.java
+++ src/by/it/drozd/jd01_05/TaskB.java
@@ -4,12 +4,12 @@ import static java.lang.Math.*;
 
 public class TaskB {
     public static void main(String[] args) {
-            double sum=0;
-        for (double a = 0; a < 2; a+=0.2) {
-
-            for (double x = 1; x <= 6; x++) {
-                sum+=x;
-            }
-        }
+//            double sum=0;
+//        for (double a = 0; a < 2; a+=0.2) {
+//
+//            for (double x = 1; x <= 6; x+=pow(7,a)) {
+//                sum+=x;
+//            }
+//        }
     }
 }
