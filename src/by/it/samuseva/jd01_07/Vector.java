d7b5e6189203b0b589cb498353388c213e0e0f0a
scokpius <scokpius@gmail.com>
2019-12-26 00:59:54 +0300

    add jd01_07 all

---
+++ src/by/it/samuseva/jd01_07/Vector.java
@@ -0,0 +1,43 @@
+package by.it.samuseva.jd01_07;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+    private double[] value;
+
+    Vector(double[] value) {
+        this.value = Arrays.copyOf(value, value.length);
+    }
+    Vector(Vector vector) {
+        this.value = vector.value;
+    }
+    Vector(String str) {
+        str.trim();
+       //  по другому не смогла удолить фигурные скобки
+        String[] string = str.split("[{ ,}]+");
+        if (string[0]!="")
+            for (int i = 0; i < string.length-1; i++) {
+                string[i]=string[i+1];
+            }
+        string = Arrays.copyOf(string,string.length-1);
+
+        double[] array = new double[string.length];
+        for (int i = 0; i < array.length; i++) {
+                array[i] = Double.parseDouble(string[i]);
+        }
+        this.value = array;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder("{");
+        String delimiter ="";
+        for (double element : value) {
+            string.append(delimiter).append(element);
+            delimiter = ", ";
+
+        }
+        string.append("}");
+        return string.toString();
+    }
+}
