3fb040d4d9c79ed7f0662cf5dc4278d13fba6979
loktev <loktev@tut.by>
2019-12-26 13:09:32 +0300

    classwork jd01_08 Level A

---
+++ src/by/it/akhmelev/jd01_08/Var.java
@@ -0,0 +1,35 @@
+package by.it.akhmelev.jd01_08;
+
+abstract class Var implements Operation {
+
+
+    @Override
+    public Var add(Var other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Operation " + this + "-" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Operation " + this + "*" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Operation " + this + "/" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "abstract Var";
+    }
+
+}
