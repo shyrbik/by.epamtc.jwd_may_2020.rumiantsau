9a7b03ffc583a0b8d52d8e2b8bc51660a5af047d
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-28 12:37:00 +0300

    A is done

--- src/by/it/popkov/jd01_08/Scalar.java
+++ src/by/it/popkov/jd01_08/Scalar.java
@@ -19,4 +19,36 @@ class Scalar extends Var {
     public String toString() {
         return Double.toString(value);
     }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar){
+            return new Scalar(this.value + ((Scalar) other).value);
+        }
+        return other.add(this);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar){
+            return new Scalar(this.value - ((Scalar) other).value);
+        }
+        return new Scalar(-1).mul(other.sub(this));
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar){
+            return new Scalar(this.value * ((Scalar) other).value);
+        }
+        return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar){
+            return new Scalar(this.value / ((Scalar) other).value);
+        }
+        return other.div(this);
+    }
 }
