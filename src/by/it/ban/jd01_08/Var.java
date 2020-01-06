b5db3c8047d5e971524fc0c0f4f751bbd9aa254b
tycoonban <9777687@gmail.com>
2020-01-06 12:13:09 +0300

    8ab

---
+++ src/by/it/ban/jd01_08/Var.java
@@ -0,0 +1,105 @@
+package by.it.ban.jd01_08;
+
+
+public abstract class Var implements Operation {
+    @Override
+    public String toString() {
+        return "Var{}";
+    }
+
+    @Override
+    public Var add(Var other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var add(Scalar other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var sub(Scalar other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var mul(Scalar other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var div(Scalar other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var add(Vector other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var sub(Vector other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var mul(Vector other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var div(Vector other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var add(Matrix other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var sub(Matrix other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var mul(Matrix other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var div(Matrix other) {
+        System.out.println("Operation " + this + "+" + other + " impossible");
+        return null;
+    }
+}
