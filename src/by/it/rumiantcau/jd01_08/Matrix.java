024abd38736b20d3f95faecb1fa98d77905ae0bb
shyrbik <shyrbik@gmail.com>
2020-01-08 15:09:40 +0300

    08 rumiantsau A+B

---
+++ src/by/it/rumiantcau/jd01_08/Matrix.java
@@ -0,0 +1,37 @@
+package by.it.rumiantcau.jd01_08;
+
+class Matrix extends Var {
+private double[][] value;
+
+Matrix(String value){
+  //  this.value = value;
+}
+Matrix(Matrix matrix){
+    this.value = matrix.value;
+}
+
+
+public String toString() {
+    StringBuilder sb=new StringBuilder("{{");
+
+    for (int i = 0; i < value.length; i++) {
+        String delimiter="";
+        for (int j = 0; j < value[0].length; j++) {
+
+            sb.append(delimiter).append(value[i][j]);
+            delimiter=", ";
+        }
+        if (i<value.length-1) sb.append("}, {");
+
+    }
+    sb.append("}}");
+
+    return sb.toString();
+    }
+
+
+
+
+
+
+}
