87d855655f93e8e63cbb51de001a249e9a4b40e1
shyrbik <shyrbik@gmail.com>
2020-01-08 15:09:40 +0300

    08 rumiantsau A+B

--- src/by/it/rumiantcau/jd01_03/InOut.java
+++ src/by/it/rumiantcau/jd01_03/InOut.java
@@ -1,8 +1,8 @@
 package by.it.rumiantcau.jd01_03;
-
+/*p
 public class InOut {
 
-    static Double[ ] getArray(String line) {
+    static Double[] getArray(String line) {
         String[] strArr=line.split(" ");
         Double[] res= new Double[strArr.length];
         for (int i = 0; i < strArr.length; i++) {
@@ -12,14 +12,14 @@ public class InOut {
 
     }
 
-    static void printArray(Double[] arr){
+    static void printArray(Double[ ] arr){
         for (double element : arr) {
             System.out.print(element+" ");
         }
         System.out.println();
     }
 
-    static void printArray(Double[] arr, String name, int columnCount){
+    static void printArray(Double[ ] arr, String name, int columnCount){
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%s[% -4d]=%-11.4f ",name,i,arr[i]);
             if ((i+1)%columnCount==0 || i+1==arr.length)
@@ -29,4 +29,38 @@ public class InOut {
     }
 
 
+}*/
+public class InOut {
+    static double [] getArray(String line){
+        String[]strArr = line.split(" ");
+        double [] res = new double[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            res[i]=  Double.parseDouble(strArr[i]);
+
+
+        }
+
+
+
+
+
+        return res;
+    }
+    static void printArray(double[ ] arr){
+        for (double element : arr) {
+            System.out.print(element+" ");
+
+        }
+        System.out.println();
+    }
+    static void printArray(double[ ] arr, String name, int columnCount){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%s[% -3d]=%-10.4f ", name,i,arr[i]);
+            if ((i+1)%columnCount==0||i+1==arr.length){
+                System.out.println();
+            }
+
+        }
+    }
 }
+
--- src/by/it/rumiantcau/jd01_04/TaskA.java
+++ src/by/it/rumiantcau/jd01_04/TaskA.java
@@ -17,9 +17,8 @@ public class TaskA {
     static void printMulTable(){
         for (int i = 2; i < 10; i++) {
             for (int j = 2; j < 10; j++) {
-           System.out.print    (i+"*"+j+"="+i*j+"   ");
-
-            //System.out.printf("1%d*1%d=%-2d ",i,j,i*j);
+          // System.out.print    (i+"*"+j+"="+i*j+"   ");
+                System.out.printf("%d*%d=%-2d ", i, j, i * j);
                             }
             System.out.println();
         }
