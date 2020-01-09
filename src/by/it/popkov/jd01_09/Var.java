c3fae6e15f553f77cf9293e1eac121b4158d569b
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-09 17:15:19 +0300

    add

---
+++ src/by/it/popkov/jd01_09/Var.java
@@ -0,0 +1,27 @@
+package by.it.popkov.jd01_09;
+
+abstract class Var implements Operation {
+    @Override
+    public Var add(Var other) {
+        System.out.println(this + " + " + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println(this + " - " + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println(this + " * " + other + " impossible");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println(this + " / " + other + " impossible");
+        return null;
+    }
+}
