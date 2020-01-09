ad8fab9929e0243e9f83e2fb019799737dd53bdd
dmitry.barkovsky <dbarkov@tut.by>
2020-01-09 06:26:44 +0300

    commit jd03-jd07

---
+++ src/by/it/barkovsky/jd01_07/Scalar.java
@@ -0,0 +1,21 @@
+package by.it.barkovsky.jd01_07;
+
+class Scalar extends Var {
+    private double value;
+    Scalar(double value) {
+        this.value=value;
+    }
+
+    Scalar(Scalar scalar) {
+        this.value=scalar.value;
+    }
+
+    public Scalar(String strScalar) {
+        this.value = Double.parseDouble(strScalar);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(this.value);
+    }
+}
