9aa071f9aaf99843eeedb391c745ba8f33d463f6
sjubusel <sju.busel@gmail.com>
2020-01-14 13:11:22 +0200

    Busiel Siarhei - JD01_09 - ABC complete

---
+++ src/by/it/busel/jd01_09/Patterns.java
@@ -0,0 +1,25 @@
+package by.it.busel.jd01_09;
+
+/**
+ * a class which contains pattern in order to find the most common expression in a Calc-prototype
+ */
+class Patterns {
+    /**
+     * A pattern (String-object) containing basic arithmetical operators corresponding to subtraction, addition,
+     * multiplication, division
+     */
+    static final String OPERATION = "[-+*/]";
+    /**
+     * A pattern (String-object) containing a general definition of a Scalar expression
+     */
+    static final String SCALAR = "-?[0-9]+([\\.][0-9]+)?";
+    /**
+     * A pattern (String-object) containing a general definition of a Vector expression
+     */
+    static final String VECTOR = "\\{" + SCALAR + "(, *" + SCALAR + ")*\\}";
+    /**
+     * A pattern (String-object) containing a general definition of a Matrix expression
+     */
+    static final String MATRIX = "\\{" + VECTOR + "(, *" + VECTOR + ")*\\}";
+
+}
--- src/by/it/busel/jd01_09/Var.java
+++ src/by/it/busel/jd01_09/Var.java
@@ -4,6 +4,25 @@ package by.it.busel.jd01_09;
  * a class that is a general specimen of a math expression
  */
 class Var implements Operation {
+    /** a method, which creates a Var expression of a certain type (Scalar, Vector, Matrix) depending on
+     * the value that contains the String-argument "operand"
+     * @param operand a String-object containing a Var expression
+     * @return a Var expression
+     */
+    static Var createVar(String operand) {
+        if (operand.matches(Patterns.SCALAR)) {
+            return new Scalar(operand);
+        } else if (operand.matches(Patterns.VECTOR)) {
+            return new Vector(operand);
+        } else if (operand.matches(Patterns.MATRIX)) {
+            return new Matrix(operand);
+        } else {
+            System.out.println("A wrong format of input expression!\nCheck the expression You inputted. Then re-enter your expression, please!");
+            return null;
+        }
+
+    }
+
     @Override
     public String toString() {
         return "exampleVar.toString()";
@@ -11,6 +30,7 @@ class Var implements Operation {
 
     /**
      * a method that informs whether a mathematical operation of addition can not be performed
+     *
      * @param other an instance of Var-class or its subclasses, which is to be added
      * @return a result of addition
      */
@@ -22,6 +42,7 @@ class Var implements Operation {
 
     /**
      * a method that informs whether a mathematical operation of subtraction can not be performed
+     *
      * @param other an instance of Var-class or its subclasses, which is to be subtracted
      * @return a result of subtraction
      */
@@ -33,6 +54,7 @@ class Var implements Operation {
 
     /**
      * a method that informs whether a mathematical operation of multiplication can not be performed
+     *
      * @param other an instance of Var-class or its subclasses, which is a multiplier
      * @return a result of a multiplication
      */
@@ -44,6 +66,7 @@ class Var implements Operation {
 
     /**
      * a method that informs whether a mathematical operation of division can not be performed
+     *
      * @param other an instance of Var-class or its subclasses, which is divisor
      * @return a result of division
      */
