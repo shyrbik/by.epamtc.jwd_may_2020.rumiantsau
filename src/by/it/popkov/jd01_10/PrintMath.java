1bc8efc9a89cb969180be69910fa2010170c6866
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-21 17:55:25 +0300

    done

---
+++ src/by/it/popkov/jd01_10/PrintMath.java
@@ -0,0 +1,35 @@
+package by.it.popkov.jd01_10;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+
+public class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> mathClass = Math.class;
+        Method[] methods = mathClass.getMethods();
+        StringBuilder sb = new StringBuilder();
+        for (Method method : methods) {
+            if (Modifier.isPublic(method.getModifiers())) {
+                sb.append("public ");
+                if (Modifier.isStatic(method.getModifiers())) {
+                    sb.append("static ");
+                }
+                sb.append(method.getReturnType().getName()).append(" ").append(method.getName()).append("(");
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                for (int i = 0; i < parameterTypes.length; i++) {
+                    sb.append(parameterTypes[i].getName());
+                    if (i != parameterTypes.length - 1) sb.append(",");
+                }
+                sb.append(")").append("\n");
+            }
+        }
+        Field[] fields = mathClass.getFields();
+        for (Field field : fields) {
+            sb.append(field.getType().getName()).append(" ").append(field.getName()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
