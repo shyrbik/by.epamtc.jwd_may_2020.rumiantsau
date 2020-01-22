47036e73310b4b247accefafea75c557f9513bf9
SlavaFilipovich <stanley.2011@mail.ru>
2020-01-22 17:16:10 +0300

    jd01_10 Tasks A,B,C completed

--- src/by/it/filipovich/jd01_10/Bean.java
+++ src/by/it/filipovich/jd01_10/Bean.java
@@ -2,7 +2,7 @@ package by.it.filipovich.jd01_10;
 
 public class Bean {
 
-    @Param(a=2, b=4)
+    @Param(a=3, b=5)
     public static double sum(int a, int b){
         return (a+b)*1.0;
     }
@@ -10,12 +10,12 @@ public class Bean {
     public static double max(int a, int b){
         return (Math.max(a,b))*1.0;
     }
-    @Param(a=3, b=4)
+    @Param(a=5, b=4)
     public double min(int a, int b){
         return (Math.min(a,b))*1.0;
     }
 
     public double avg(int a, int b){
-        return (double)(a+b)/2.0;
+        return (a+b)/2.0;
     }
 }
--- src/by/it/filipovich/jd01_10/BeanTester.java
+++ src/by/it/filipovich/jd01_10/BeanTester.java
@@ -1,7 +1,21 @@
 package by.it.filipovich.jd01_10;
 
-public class BeanTester {
-    public static void main(String[] args) {
+import java.lang.reflect.Method;
 
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+        Bean bean = Bean.class.newInstance();
+        Method[] declaredMethods = Bean.class.getDeclaredMethods();
+
+        for (Method declaredMethod : declaredMethods) {
+            if(declaredMethod.isAnnotationPresent(Param.class)) {
+                Param annotation = declaredMethod.getAnnotation(Param.class);
+                int a = annotation.a();
+                int b = annotation.b();
+                Object invoke = declaredMethod.invoke(bean,a,b);
+                System.out.println(declaredMethod.getName());
+                System.out.println(invoke.toString());
+            }
+        }
     }
 }
