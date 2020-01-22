a095b6f3ea1c299d8611568b962f2cbb0a25336a
sjubusel <sju.busel@gmail.com>
2020-01-22 22:39:16 +0200

    Busiel Siarhei - JD01_10 - Level A complete

---
+++ src/by/it/busel/jd01_10/Bean.java
@@ -0,0 +1,22 @@
+package by.it.busel.jd01_10;
+
+public class Bean {
+    @Param(a = 2, b = 5)
+    static double sum(int a, int b) {
+        return (double) (a + b);
+    }
+
+    @Param(a = 2, b = 5)
+    double max(int a, int b) {
+        return a > b ? (double) a : (double) b;
+    }
+
+    @Param(a = 2, b = 5)
+    double min(int a, int b) {
+        return a < b ? (double) a : (double) b;
+    }
+
+    static double avg(int a, int b) {
+        return ((double) (a + b)) / 2;
+    }
+}
