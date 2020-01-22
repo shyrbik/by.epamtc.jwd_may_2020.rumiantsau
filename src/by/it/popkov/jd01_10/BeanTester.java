e6a537ad4f21380a5179bf009dbcde2395c20f18
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-22 13:50:54 +0300

    done

--- src/by/it/popkov/jd01_10/BeanTester.java
+++ src/by/it/popkov/jd01_10/BeanTester.java
@@ -7,15 +7,16 @@ public class BeanTester {
         Class<Bean> beanClass = Bean.class;
         Bean bean = beanClass.newInstance();
         Method[] methods = beanClass.getDeclaredMethods();
+        StringBuilder sb = new StringBuilder();
         for (Method method : methods) {
             if (method.isAnnotationPresent(Param.class)) {
-                StringBuilder sb = new StringBuilder();
                 Param annotation = method.getAnnotation(Param.class);
-                sb.append(method.getName()).append(" ").append(method.invoke(bean, annotation.a(), annotation.b()));
-                System.out.println(sb);
+                sb.append(method.getName())
+                        .append(" ")
+                        .append(method.invoke(bean, annotation.a(), annotation.b()))
+                        .append("\n");
             }
         }
-
-
+        System.out.println(sb);
     }
 }
