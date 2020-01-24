ab96a4061a085e9bffa68ae72d6455ec334a0cd0
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-24 12:46:13 +0300

    Improved

--- src/by/it/popkov/calc/Parser.java
+++ src/by/it/popkov/calc/Parser.java
@@ -13,6 +13,10 @@ class Parser {
             String[] values = expression.split(Patterns.MATH_SIGN);
             String first = values[0];
             String second = values[1];
+            if (sing.equals("=")){
+                Var.save(values[0],Var.newVar(second));
+                return Var.newVar(second);
+            }
             if (Var.newVar(first) != null && Var.newVar(second) != null) {
                 switch (sing) {
                     case "+":
--- src/by/it/popkov/calc/Var.java
+++ src/by/it/popkov/calc/Var.java
@@ -1,6 +1,12 @@
 package by.it.popkov.calc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 abstract class Var implements Operation {
+
+    private static Map<String, Var> valueMap = new HashMap<>();
+
     @Override
     public Var add(Var other) {
         System.out.println(this + " + " + other + " impossible");
@@ -29,6 +35,10 @@ abstract class Var implements Operation {
         if (strVar.matches(Patterns.SCALAR)) return new Scalar(strVar);
         else if (strVar.matches(Patterns.VECTOR)) return new Vector(strVar);
         else if (strVar.matches(Patterns.MATRIX)) return new Matrix(strVar);
-        else return null;
+        else return valueMap.get(strVar);
+    }
+
+    public static void save(String value, Var value1) {
+        valueMap.put(value, value1);
     }
 }
