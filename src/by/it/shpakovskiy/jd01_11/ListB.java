cbd068cb5b78bc479519fafee088a52732cb6dd2
ilya <ilyaliberty@gmail.com>
2020-01-27 16:30:10 +0300

    jd01_11: task 1

---
+++ src/by/it/shpakovskiy/jd01_11/ListB.java
@@ -0,0 +1,214 @@
+package by.it.shpakovskiy.jd01_11;
+
+import java.util.*;
+
+class ListB<T> implements List<T> {
+    private T[] elements = (T[]) new Object[]{};
+    private int size = 0;
+
+    @Override
+    public boolean add(T t) {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
+        }
+        elements[size++] = t;
+        return true;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) {
+        T[] elementsOfCopy = (T[]) collection.toArray();
+        int count = elementsOfCopy.length;
+        if (count == 0) {
+            return false;
+        } else {
+            if (size+count>=elements.length){
+                elements = Arrays.copyOf(elements, elements.length + count);
+            }
+            System.arraycopy(elementsOfCopy, 0, elements, size, count);
+            size += count;
+            return true;
+        }
+    }
+
+    @Override
+    public T remove(int index) {
+        T element = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
+        return element;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index > -1) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public T get(int index) {
+        return elements[index];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder text = new StringBuilder("[");
+        String delimiter = "";
+        for (int i = 0; i < size; i++) {
+            text.append(delimiter).append(elements[i]);
+            delimiter = ", ";
+        }
+        text.append("]");
+        return text.toString();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T set(int i, T element) {
+        if (i <= size) {
+            T oldElement = elements[i];
+            elements[i] = element;
+            return oldElement;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) {
+        return null;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        T[] elementsOfRemove = (T[]) collection.toArray();
+        int count = elementsOfRemove.length;
+        if (count == 0) {
+            return false;
+        } else {
+            for (T t : elementsOfRemove) {
+                for (int j = 0; j < size; j++) {
+                    if (t == elements[j]) {
+                        remove(j);
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        T[] elementsOfRemove = (T[]) collection.toArray();
+        if (elementsOfRemove.length == 0) {
+            return false;
+        } else {
+            for (int i = 0; i < size; i++) {
+                boolean isElement=false;
+                for (T t : elementsOfRemove) {
+                    if (elements[i] == t) {
+                        isElement = true;
+                        break;
+                    }
+                }
+                if (!isElement){
+                    remove(i);
+                }
+            }
+            return true;
+        }
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int i, int i1) {
+        return null;
+    }
+}
