13ae9e44c836eaa9c734c06135a43a3fa3ef8bc6
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-28 21:30:22 +0300

    Improvement

--- src/by/it/popkov/jd01_12/TaskB1.java
+++ src/by/it/popkov/jd01_12/TaskB1.java
@@ -7,18 +7,14 @@ public class TaskB1 {
     public static void main(String[] args) {
         HashMap<String, Integer> wordNumber = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
         while (true) {
             String row = scanner.next();
             if (row.equals("end")) break;
-            sb.append(row).append(" ");
-        }
-        String[] split = sb.toString().trim().split("[— /!?:;,.]+");
-        for (String s : split) {
-            if (!wordNumber.containsKey(s)) {
-                wordNumber.put(s, 1);
-            } else {
-                wordNumber.put(s, wordNumber.get(s) + 1);
+            row = row.replaceAll("[—/!?:;,.)]+", "");
+            if (!wordNumber.containsKey(row)) {
+                wordNumber.put(row, 1);
+            } else if (row.length()>0) {
+                wordNumber.put(row, wordNumber.get(row) + 1);
             }
         }
         System.out.println(wordNumber);
