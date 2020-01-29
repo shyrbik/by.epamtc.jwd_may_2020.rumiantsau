08f6f83e991c330df672e4fcd11526c76552203c
Andygo298 <andygo298@gmail.com>
2020-01-29 21:06:47 +0300

    Task B2

--- src/by/it/lozouski/jd01_12/TaskB1.java
+++ src/by/it/lozouski/jd01_12/TaskB1.java
@@ -17,13 +17,13 @@ class TaskB1 {
             Matcher matcher = myRegexPattern.matcher(text); // создадим матчер для текста
             while (matcher.find()) { //...пока матчер что-то нашел
                 String word = matcher.group();// присвоим найденное переменной
-                if (word.equals("end")) {//если word совпадает с end
-                    flag = true; //...флаг на true,чтобы свернуть внешний цикл while
+                if (word.equals("end")) {//если word совпадает с end, то:
+                    flag = true; // ставим флаг на true,чтобы внешний цикл while больше не запускался
                     break; //.. и сворачиваем текуший while
-                }
+                }//если условие не прошло, тогда:
                 Integer value = myMap.get(word); // получим value по ключу word(найденое матчером по regex слово)
-                if (value == null) myMap.put(word, 1); //если еще не встречалось, запишем в value = 1 (кол-во раз)
-                else myMap.put(word, value + 1); // если встречалось...добавим в value +1
+                if (value == null) myMap.put(word, 1); //если еще не встречалось, запишем в мапу слово и value = 1
+                else myMap.put(word, value + 1); // если встречалось...просто перезапишем это слово и в value добавим +1
             }
         }
         //распечатка мапы : myMap.entrySet().for
