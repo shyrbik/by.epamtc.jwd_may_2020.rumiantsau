4241a73f74236e35e8b21434d2ed558e2064fb95
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-29 12:37:25 +0300

    Done

---
+++ src/by/it/popkov/jd01_12/TaskB2.java
@@ -0,0 +1,44 @@
+package by.it.popkov.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class TaskB2 {
+    static String process(ArrayList<String> peoples) {
+        ArrayList<String> copy = new ArrayList<>(peoples);
+        boolean a = false;
+        while (copy.size() > 1) {
+            Iterator<String> iterator = copy.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                if (a) {
+                    iterator.remove();
+                    a = false;
+                } else a = true;
+            }
+        }
+        return copy.get(0);
+    }
+
+    static String process(LinkedList<String> peoples) {
+        LinkedList<String> copy = new LinkedList<>(peoples);
+        boolean a = false;
+        while (copy.size() > 1) {
+            Iterator<String> iterator = copy.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                if (a) {
+                    iterator.remove();
+                    a = false;
+                } else a = true;
+            }
+        }
+        return copy.get(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(process(new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"))));
+    }
+}
