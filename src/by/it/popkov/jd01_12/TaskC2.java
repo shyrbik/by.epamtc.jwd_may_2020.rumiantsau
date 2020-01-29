e9215e204fe8117aba8dcb96c7ee415989337af5
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-01-29 16:07:29 +0300

    Done

--- src/by/it/popkov/jd01_12/TaskC2.java
+++ src/by/it/popkov/jd01_12/TaskC2.java
@@ -20,11 +20,12 @@ public class TaskC2 {
     }
 
     public static void main(String[] args) {
-        Set<Integer> a = new HashSet<>(Arrays.asList(23, 6, 1, 6, 2, 21, 7, 8, 24, 2));
+        Set<Double> d = new HashSet<>(Arrays.asList(23.0, 6.0, 1.0, 6.0, 2.0, 21.0, 7.0, 8.0, 24.0, 2.0));
+        Set<Long> l = new HashSet<>(Arrays.asList(1L, 2L, 3L, 24L, 34L, 23L, 999L, 4L, 34L, 1234L, 5L, 2L, 14L, 777L, 5L));
         Set<Integer> b = new HashSet<>(Arrays.asList(3, 56, 14, 63, 2, 21, 75, 8, 24, 2));
         Set<Integer> c = new HashSet<>(Arrays.asList(23, 6, 1, 65, 2, 21, 7, 85, 2434, 2));
-        System.out.println(getCross(a, b, c));
-        System.out.println(getUnion(a, b, c));
+        System.out.println(getCross(d, l, b, c));
+        System.out.println(getUnion(d, l, b, c));
 
     }
 }
