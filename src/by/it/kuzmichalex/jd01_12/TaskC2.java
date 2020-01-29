9cf31efbb50e27d4a2db70b0ba494864d54b3f27
kuzmichalex <58950628+kuzmichalex@users.noreply.github.com>
2020-01-29 13:11:37 +0300

    Task 12 A B C

--- src/by/it/kuzmichalex/jd01_12/TaskC2.java
+++ src/by/it/kuzmichalex/jd01_12/TaskC2.java
@@ -1,15 +1,17 @@
 package by.it.kuzmichalex.jd01_12;
 
 import java.util.*;
+
 public class TaskC2 {
 
     /**
      * Метод для определения пересечения N множеств
+     *
      * @param - Э! А что подстказа не выскакивает? Короче, передавайте множество множеств
      * @return множество, являющееся пересечением переданных. Порядок абы-какой.
      */
     @SuppressWarnings("all")
-    static Set<?> getCross(Set<?> setStart, Set<?>...sets) {
+    static Set<?> getCross(Set<?> setStart, Set<?>... sets) {
         Set<?> returnSet = new HashSet<>(setStart);
         for (Set<?> set : sets) {
             returnSet.retainAll(set);
@@ -18,6 +20,8 @@ public class TaskC2 {
     }
 
     /**
+     *
+     *
      * @param - так и не дожэдался подсказки. А без неё не можется. Эта самая ителижиидея делает из людей лентяев!
      * @return множество, являющееся объединением переданных. С натуральной, без ГМО, сортировкой!
      */
@@ -40,22 +44,21 @@ public class TaskC2 {
 
         Set<?> crossSet = getCross(a_int, b_int);
         System.out.println("cross(2 args): " + crossSet);
-        Set<?> unionSet = getUnion(a_int,b_int);
+        Set<?> unionSet = getUnion(a_int, b_int);
         System.out.println("union(2 args): " + unionSet);
 
         System.out.println("-------------------------testing witch double");
         Set<? extends Number> a_double = new HashSet<>(Arrays.asList(0.0, 1.0, 1.0, 3003.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 12.0, 11.0, 10.0, 1001.0));
         Set<? extends Number> b_double = new TreeSet<>(Arrays.asList(19.0, 18.0, 17.0, 16.0, 15.0, 15.0, 1001.0, 15.0, 14.0, 13.0, 12.0, 11.0, 10.0, 2002.0));
-        Set<? extends Number> c_double = new TreeSet<>(Arrays.asList(11.0, 22.2,33.3,44.4));
+        Set<? extends Number> c_double = new TreeSet<>(Arrays.asList(11.0, 12.0, 22.2, 33.3, 44.4));
 
         System.out.println("HashSet : " + a_double);
         System.out.println("TreeSet1: " + b_double);
         System.out.println("TreeSet2: " + c_double);
         crossSet = getCross(a_double, b_double, c_double);
         System.out.println("cross(3 args): " + crossSet);
-        unionSet = getUnion(a_double,b_double, c_double);
+        unionSet = getUnion(a_double, b_double, c_double);
         System.out.println("union(3 args): " + unionSet);
-
 
 
     }
