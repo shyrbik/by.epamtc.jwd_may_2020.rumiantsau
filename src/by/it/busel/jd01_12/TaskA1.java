754ec89f13941e818e9e6709d4cc41ebf2358a93
sjubusel <sju.busel@gmail.com>
2020-01-29 23:40:33 +0200

    Busiel Siarhei -  JD01_12 - A2 complete

--- src/by/it/busel/jd01_12/TaskA1.java
+++ src/by/it/busel/jd01_12/TaskA1.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-public class TaskA1 {
+class TaskA1 {
     private List<Integer> pupilsGrades = new ArrayList<Integer>();
 
     void clearBad(List<Integer> grades) {
