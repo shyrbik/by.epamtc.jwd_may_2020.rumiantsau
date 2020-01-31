3fa572f743aced419d1ec9acee97cd72a1d14930
SlavaFilipovich <stanley.2011@mail.ru>
2020-01-31 22:34:15 +0300

    jd01_13 Tasks A,B,C completed

--- src/by/it/filipovich/jd01_13/TaskC.java
+++ src/by/it/filipovich/jd01_13/TaskC.java
@@ -3,45 +3,45 @@ package by.it.filipovich.jd01_13;
 import java.util.*;
 
 class TaskC {
-    private static List<Double> list = new LinkedList<>();
+    static List<Double> list = new LinkedList<>();
+    static Scanner scannerOne;
 
-    public static void main(String[] args) {
-
-        Scanner scannerOne = new Scanner(System.in);
-        //readData();
-        for (int i = 0; i < 3; i++) {
-            String next = scannerOne.next();
-            double v = Double.parseDouble(next);
-            list.add(v);
-        }
-        System.out.println(list.toString());
-        Collections.reverse(list);
-        System.out.println(list);
-
+    public static void main(String[] args) throws InterruptedException {
+        scannerOne = new Scanner(System.in);
+        readData();
     }
 
-
-//    static void readData() throws InterruptedException {
-//
-//        try {
-//
-//            double v = Double.parseDouble(next);
-//            list.add(number);
-//
-//        } catch (IOException e) {
-//            Thread.sleep(100);
-//            System.out.println(Collections.reverse(list);
-//
-//        }
-//
-//    }
+    static void readData() throws InterruptedException {
+        TaskC taskC = new TaskC();
+        int count = 0;
+        boolean a=true;
+        while (a) {
+            try {
+                String next = scannerOne.next();
+                double number = Double.parseDouble(next);
+                list.add(number);
+            } catch (NumberFormatException e) {
+                Thread.sleep(100);
+                Collections.reverse(list);
+                System.out.println(taskC);
+                Collections.reverse(list);
+                count++;
+            } finally {
+                if (count == 5) {
+                    Collections.reverse(list);
+                    System.out.println(taskC);
+                    throw new InterruptedException();
+                }
+            }
+        }
+    }
 //
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("");
         String del = "";
         for (Double aDouble : list) {
-            sb.append(del).append(aDouble);
+            sb.append(del).append((double) aDouble);
             del = " ";
         }
         return sb.toString();
