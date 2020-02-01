b9bcbf80d92ed8ca66d49e27a48b8ce90816bb94
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-01 15:21:54 +0300

    Done

--- src/by/it/popkov/jd01_12/TaskC2.java
+++ src/by/it/popkov/jd01_12/TaskC2.java
@@ -1,7 +1,7 @@
 package by.it.popkov.jd01_12;
 
 import java.util.*;
-//?????????????????Добавить привидение к отдному типу, как если бы 2.0, 2L, 2 были бы одним и тем же
+
 class TaskC2 {
     private static Set getCross(Set<? extends Number>... many) {
         Set<Number> outPut = new HashSet<>(many[0]);
@@ -19,13 +19,25 @@ class TaskC2 {
         return outPut;
     }
 
+    private static Set castToDouble(Set<? extends Number> start) {
+        double[] doubles = start.stream().mapToDouble(Number::doubleValue).toArray();
+        HashSet<Double> d = new HashSet<>();
+        for (double aDouble : doubles) {
+            d.add(aDouble);
+        }
+        return d;
+    }
+
     public static void main(String[] args) {
-        Set<Double> d = new HashSet<>(Arrays.asList(23.0, 6.0, 1.0, 6.0, 2.0, 21.0, 7.0, 8.0, 24.0, 2.0));
-        Set<Long> l = new HashSet<>(Arrays.asList(1L, 2L, 3L, 24L, 34L, 23L, 999L, 4L, 34L, 1234L, 5L, 2L, 14L, 777L, 5L));
-        Set<Integer> b = new HashSet<>(Arrays.asList(3, 56, 14, 63, 2, 21, 75, 8, 24, 2));
-        Set<Integer> c = new HashSet<>(Arrays.asList(23, 6, 1, 65, 2, 21, 7, 85, 2434, 2));
+        Set<Double> d = new HashSet<>(Arrays.asList(23.0, 6.0, 1.0, 6.0, 2.0, 21.0, 7.0, 8.0, 777.0, 24.0, 2.0));
+        Set<Long> l = new HashSet<>(Arrays.asList(1L, 2L, 3L, 24L, 34L, 23L, 999L, 4L, 34L, 7755555555555555555L, 5L, 2L, 14L, 777L, 5L));
+        Set<Integer> b = new HashSet<>(Arrays.asList(3, 56, 14, 63, 2, 21, 777, 8, 24, 2));
+        Set<Integer> c = new HashSet<>(Arrays.asList(23, 6, 1, 65, 2, 21, 7, 777, 2434, 2));
+        System.out.println("Для HashSet Double 2.0, Long 2L, Integer 2 это разные числа");
         System.out.println(getCross(d, l, b, c));
         System.out.println(getUnion(d, l, b, c));
-
+        System.out.println("\nА тут мы предварительно закастили те же Long и Integer в Double");
+        System.out.println(getCross(castToDouble(d), castToDouble(l), castToDouble(b), castToDouble(c)));
+        System.out.println(getUnion(castToDouble(d), castToDouble(l), castToDouble(b), castToDouble(c)));
     }
 }
