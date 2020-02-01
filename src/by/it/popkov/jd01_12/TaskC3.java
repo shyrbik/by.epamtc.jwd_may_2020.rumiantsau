dec971d7a6dc9cc11fc6b1e57d6cb8cf6d5a71f1
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-01 14:12:55 +0300

    Done

--- src/by/it/popkov/jd01_12/TaskC3.java
+++ src/by/it/popkov/jd01_12/TaskC3.java
@@ -5,52 +5,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class TaskC3 {
-
-
     static boolean check(String row) {
-        Deque<String> deque = new LinkedList<>();
+        List<String> list = new LinkedList<>();
         Pattern p = Pattern.compile("[\\[\\](){}]");
         Matcher m = p.matcher(row);
         while (m.find()) {
-            deque.add(m.group());
+            list.add(m.group());
         }
-        while (deque.size() > 0) { // Удаляем правильные пары пока не будет 0 size
-            switch (deque.getFirst()) {
-                case "{":
-                    if (deque.getLast().equals("}")) { // Если две крайние соответсвуют {[()]}
-                        deque.removeFirst();
-                        deque.removeLast();
-                    } else { //Если две крайние не правильны, проверяем идёт ли за первой сразу закрывающая её {}([])
-                        deque.removeFirst();
-                        if (deque.getFirst().equals("}")) deque.removeFirst();
-                        else return false;
-                    }
-                    break;
-
-                case "[":
-                    if (deque.getLast().equals("]")) {
-                        deque.removeFirst();
-                        deque.removeLast();
-                    } else {
-                        deque.removeFirst();
-                        if (deque.getFirst().equals("]")) deque.removeFirst();
-                        else return false;
-                    }
-                    break;
-
-                case "(":
-                    if (deque.getLast().equals(")")) {
-                        deque.removeFirst();
-                        deque.removeLast();
-                    } else {
-                        deque.removeFirst();
-                        if (deque.getFirst().equals(")")) deque.removeFirst();
-                        else return false;
-                    }
-                    break;
-
-                default:
-                    return false;
+        while (list.size() > 0) { // Удаляем правильные пары пока не будет 0 size
+            if ((list.get(0).equals("{") && list.get(1).equals("}"))
+                    || (list.get(0).equals("(") && list.get(1).equals(")"))
+                    || (list.get(0).equals("[") && list.get(1).equals("]"))) {
+                list.remove(1);
+                list.remove(0);
+            } else if (((list.get(list.size() - 2)).equals("{") && list.get(list.size() - 1).equals("}"))
+                    || ((list.get(list.size() - 2)).equals("(") && list.get(list.size() - 1).equals(")"))
+                    || ((list.get(list.size() - 2)).equals("[") && list.get(list.size() - 1).equals("]"))) {
+                list.remove(list.size() - 1);
+                list.remove(list.size() - 2);
+            } else if ((list.get(0).equals("{") && list.get(list.size() - 1).equals("}"))
+                    || (list.get(0).equals("(") && list.get(list.size() - 1).equals(")"))
+                    || (list.get(0).equals("[") && list.get(list.size() - 1).equals("]"))) {
+                list.remove(list.size() - 1);
+                list.remove(0);
+            } else {
+                return false;
             }
         }
         return true;
@@ -60,59 +39,5 @@ class TaskC3 {
         Scanner scanner = new Scanner(System.in);
         String row = scanner.nextLine();
         System.out.println(check(row));
-
-
-//        Map<String, Integer> map = new HashMap<>();
-//        map.put("(", 0);
-//        map.put(")", 0);
-//        map.put("{", 0);
-//        map.put("}", 0);
-//        map.put("[", 0);
-//        map.put("]", 0);
-//        Scanner scanner = new Scanner(System.in);
-//        String row = scanner.nextLine();
-//        Pattern p = Pattern.compile("[\\[\\](){}]");
-//        Matcher m = p.matcher(row);
-//        while (m.find()) {
-//            map.put(m.group(), map.get(m.group()) + 1);
-//        }
-//        if (map.get(")").equals(map.get("(")) && map.get("[").equals(map.get("]")) && map.get("{").equals(map.get("}")))
-//            System.out.println("true");
-//        else System.out.println("false");
-
-
     }
-//    private static boolean check(String row) { //Будем ходить итераторам пока не удалим все пары или false
-//        List<String> linkedList = new ArrayList<>();
-//        Pattern p = Pattern.compile("[\\[\\](){}]");
-//        Matcher m = p.matcher(row);
-//        while (m.find()) {
-//            linkedList.add(m.group());
-//        }
-//        for (int i = 0; i < linkedList.size(); ) {
-//            String first = linkedList.get(i);
-//            if (first.equals("}") || first.equals(")") || first.equals("]")) return false; //Стразу нет
-//            for (String second : linkedList) {
-//
-//                if (first.equals("{") && second.equals("}")) {
-//                    linkedList.remove(i);
-//                    linkedList.remove(second);
-//                    i--;
-//                    break;
-//                } else if (first.equals("(") && second.equals(")")) {
-//                    linkedList.remove(i);
-//                    linkedList.remove(second);
-//                    i--;
-//                    break;
-//                } else if (first.equals("[") && second.equals("]")) {
-//                    linkedList.remove(i);
-//                    linkedList.remove(second);
-//                    i--;
-//                    break;
-//                }
-//            }
-//            i++;
-//        }
-//        return linkedList.size() <= 0;
-//    }
 }
