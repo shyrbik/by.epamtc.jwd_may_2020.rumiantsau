ed2607fb1f4e473a785570c2db70f7543d32a153
tycoonban <9777687@gmail.com>
2020-02-02 13:07:07 +0300

    13abc+calc

--- src/by/it/ban/calc/ConsoleRunner.java
+++ src/by/it/ban/calc/ConsoleRunner.java
@@ -12,8 +12,13 @@ public class ConsoleRunner {
             String expression = scanner.next();
             if (expression.equals("end"))
                 break;
-            Var result = parser.calc(expression);
-            if (result!=null) printer.print(result);
+            Var result = null;
+            try {
+                result = parser.calc(expression);
+                printer.print(result);
+            } catch (CalcException  e){
+                System.out.println(e.getMessage());
+            }
         }
 
 
