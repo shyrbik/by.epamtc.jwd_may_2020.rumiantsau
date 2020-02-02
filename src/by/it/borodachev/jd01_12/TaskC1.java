8658cfe6037e8f92dd01a31689cad4f05821d0f5
Borodachevi <Figvam_123>
2020-02-02 15:11:05 +0300

    JD_01_12 ALL

--- src/by/it/borodachev/jd01_12/TaskC1.java
+++ src/by/it/borodachev/jd01_12/TaskC1.java
@@ -6,23 +6,24 @@ import java.util.Scanner;
 
 public class TaskC1 {
     public static void main(String[] args) {
-        Map<String, Integer> field = new HashMap<>();
+        Map<Integer, String> field = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         Integer key = 0;
         String word = scanner.next();
         while (!word.equals("end")) {
-            field.put(word, key++);
+            field.put(key++,word);
             word = scanner.next();
         }
-        Map<Integer, String> ret = new HashMap<>();
-        for (Map.Entry<String, Integer> fieldSet : field.entrySet()) {
+        Map<String, Integer> ret = new HashMap<>();
+        for (Map.Entry<Integer, String> fieldSet : field.entrySet()) {
             // Будем оставлять только первое вхождение . без проверки остается только последнее
              if (!ret.containsKey(fieldSet.getValue())) {
                  ret.put(fieldSet.getValue(), fieldSet.getKey());
              }
         }
-        for (Map.Entry<Integer, String> retSet : ret.entrySet()) {
+        for (Map.Entry<String,Integer> retSet : ret.entrySet()) {
             System.out.println(retSet.getValue() + "=" + retSet.getKey());
         }
     }
 }
+
