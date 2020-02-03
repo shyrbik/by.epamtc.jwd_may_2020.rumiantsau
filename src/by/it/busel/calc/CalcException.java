397ff83d3d872ba0bc480b3095dda27cf8f6ebcd
sjubusel <sju.busel@gmail.com>
2020-02-03 11:27:57 +0200

    Busiel Siarhei -  JD01_13_calc: add CalcException.java

---
+++ src/by/it/busel/calc/CalcException.java
@@ -0,0 +1,18 @@
+package by.it.busel.calc;
+
+public class CalcException extends Exception {
+    public CalcException() {
+    }
+
+    public CalcException(String message) {
+        super(message);
+    }
+
+    public CalcException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CalcException(Throwable cause) {
+        super(cause);
+    }
+}
