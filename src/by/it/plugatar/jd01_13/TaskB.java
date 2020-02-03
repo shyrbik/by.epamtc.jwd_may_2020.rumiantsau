221a4bfd4fbea4f2c6c4542c209d6e5222dc51e4
henadzi.pluhatar <henadzi.pluhatar@gmail.com>
2020-02-03 06:01:43 +0300

    [+]: Parts of Tasks for jd01_13;

---
+++ src/by/it/plugatar/jd01_13/TaskB.java
@@ -0,0 +1,15 @@
+package by.it.plugatar.jd01_13;
+
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+        double d=-1;
+        double sqrt=Math.sqrt(d);
+        if (Double.isNaN(sqrt)){
+            throw new ArithmeticException("dhjdshgf");
+        }
+        Scanner scanner=new Scanner(System.in);
+        String strSome=scanner.nextLine();
+    }
+}
