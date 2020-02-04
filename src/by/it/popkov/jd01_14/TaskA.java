95eeab99492d5565091b22628dc606502ae2d023
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-04 17:35:08 +0300

    Add

--- src/by/it/popkov/jd01_14/TaskA.java
+++ src/by/it/popkov/jd01_14/TaskA.java
@@ -1,9 +1,18 @@
 package by.it.popkov.jd01_14;
 
-import java.io.File;
+import java.io.*;
+import java.util.Random;
 
 class TaskA {
     public static void main(String[] args) {
-        String java = System.getProperty("user.dir");
+        String f = Support.fileFullName(TaskA.class, "dataTaskA.bin");
+        try (DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f)))) {
+            for (int i = 0; i < 20; i++) {
+                Random random = new Random();
+                dos.writeInt(random.nextInt());
+            }
+        } catch (IOException e) {
+            System.out.println("Точно в цель, но не в ту");
+        }
     }
 }
