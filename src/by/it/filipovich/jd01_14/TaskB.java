38f4090020c14b49716810d58537bcf3c8b2357b
SlavaFilipovich <stanley.2011@mail.ru>
2020-02-05 23:03:38 +0300

    jd01_14 Tasks A,B

--- src/by/it/filipovich/jd01_14/TaskB.java
+++ src/by/it/filipovich/jd01_14/TaskB.java
@@ -1,15 +1,37 @@
 package by.it.filipovich.jd01_14;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class TaskB {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder("");
+        String fileNameOne = Helper.pathToFile("text.txt", TaskB.class);
+        String fileNameTwo = Helper.pathToFile("resultTaskB.txt", TaskB.class);
 
-        int symbols = getSymbols("text.txt");
-        int words = getWords("text.txt");
+
+        List<String> lines = Files.readAllLines(Paths.get(fileNameOne), StandardCharsets.UTF_8);
+        for(String line: lines){
+            sb.append(line).append(" ");
+        }
+
+        int symbols = getSymbols(sb.toString());
+        int words = getWords(sb.toString());
         System.out.println("words="+words+", punctuation marks="+symbols);
+        try (PrintWriter writer = new PrintWriter(fileNameTwo);){
+            writer.print("words="+words+", punctuation marks="+symbols);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
+
+
 
     private static int getWords(String s) {
         int countWord = 0;
@@ -23,6 +45,7 @@ public class TaskB {
 
     private static int getSymbols(String s) {
         int countSymbol = 0;
+        s=s.replaceAll("\n"," ").replaceAll(" {1,}","");
         Pattern pattern = Pattern.compile("[^а-яА-ЯёЁ]+");
         Matcher matcher = pattern.matcher(s);
         while (matcher.find()){
