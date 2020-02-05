94b9600cbcb602a131b41baad5034b73b8c5ffbe
tarasevich <yuliya6149116@gmail.com>
2020-02-05 13:32:07 +0300

    Task11 A Complete

---
+++ src/by/it/tarasevich/jd01_12/TaskB1.java
@@ -0,0 +1,20 @@
+package by.it.tarasevich.jd01_12;
+
+
+;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+
+        Map<String, Integer> num = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        String str;
+        while (!(str = scanner.nextLine()).equals("end")) {
+
+
+        }
+    }
+}
