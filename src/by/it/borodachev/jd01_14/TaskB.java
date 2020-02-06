1e78c34ec917b5489391622801557a6644c78555
Borodachevi <Figvam_123>
2020-02-06 10:17:31 +0300

    3

--- src/by/it/borodachev/jd01_14/TaskB.java
+++ src/by/it/borodachev/jd01_14/TaskB.java
@@ -21,7 +21,7 @@ public class TaskB {
                 String[] wordArr = strSome.replace("\r\n", " ").trim().split(" ");
                // words += wordArr.length; не учитывает несколько пробелов подрядю Переделаем на более медленный вариант
                 for (String s : wordArr) {
-                   if (s.trim().length() >0) {words++;}
+                   if (s.length() >0) {words++;}
                }
 
             }
