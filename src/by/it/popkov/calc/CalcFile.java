4855585e5deddafbc2a28b5f2f20402cdf7bbc39
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-06 19:44:47 +0300

    Add

--- src/by/it/popkov/calc/CalcFile.java
+++ src/by/it/popkov/calc/CalcFile.java
@@ -1,5 +1,15 @@
 package by.it.popkov.calc;
 
+import java.io.File;
+
 class CalcFile {
-    static private String fullFileName = Support.fileFullName(Var.class, "value.txt");
+    static private String fullFileName = fileFullName(Var.class, "value.txt");
+
+    public static String fileFullName(Class<?> taskClass, String fileName) {
+        String projectDir = System.getProperty("user.dir");
+        String classPath = taskClass.getName()
+                .replace(taskClass.getSimpleName(), "")
+                .replace(".", File.separator);
+        return projectDir + File.separator + "src" + File.separator + classPath + fileName;
+    }
 }
