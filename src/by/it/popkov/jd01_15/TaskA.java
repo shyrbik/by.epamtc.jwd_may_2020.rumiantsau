b0ca2c9d7283597b8561ce1bd6de84ae326ad80a
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-07 14:58:16 +0300

    TaskA is done

--- src/by/it/popkov/jd01_15/TaskA.java
+++ src/by/it/popkov/jd01_15/TaskA.java
@@ -1,4 +1,57 @@
 package by.it.popkov.jd01_15;
 
+import java.io.*;
+
 class TaskA {
+    public static void main(String[] args) {
+        String fileName = Support.fileFullName(TaskA.class, "matrix.txt");
+        int[][] matrix = new int[6][4];
+        matrixBuilder(matrix);
+        matrixWriter(matrix, fileName);
+        matrixReader(fileName);
+    }
+
+    private static void matrixReader(String fileName) {
+        File file = new File(fileName);
+        StringBuilder sb = new StringBuilder();
+        byte[] bytes = new byte[1000];
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
+            int b = 0;
+            while ((b = randomAccessFile.read()) >= 0){
+                sb.append((char)b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sb.toString());
+
+    }
+
+    private static void matrixWriter(int[][] matrix, String fileName) {
+        try (PrintWriter printWriter = new PrintWriter(fileName)) {
+            for (int[] ints : matrix) {
+                for (int anInt : ints) {
+                    printWriter.printf("%3d ", anInt);
+                }
+                printWriter.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void matrixBuilder(int[][] matrix) {
+        boolean min = false;
+        boolean max = false;
+        for (int[] row : matrix) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = -15 + (int) (Math.random() * 31);
+                if (row[i] == 15) max = true;
+                else if (row[i] == -15) min = true;
+            }
+        }
+        if (!max || !min) matrixBuilder(matrix);
+    }
 }
