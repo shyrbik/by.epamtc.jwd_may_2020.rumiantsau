137b108c124a6d11e13984b5c3e7e498543ef6ad
Andygo298 <andygo298@gmail.com>
2020-02-09 22:38:42 +0300

    calc started

--- src/by/it/lozouski/calc/ConsoleRunner.java
+++ src/by/it/lozouski/calc/ConsoleRunner.java
@@ -1,13 +1,15 @@
 package by.it.lozouski.calc;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 class ConsoleRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CalcException {
         Scanner sc1 = new Scanner(System.in);
         String inputLine;
         Parser parser = new Parser();
         Printer printer = new Printer();
+        VarFile.load(new Parser());
         while (!(inputLine = sc1.nextLine()).equals("end")) {
             try {
                 Var result = parser.calculate(inputLine);
