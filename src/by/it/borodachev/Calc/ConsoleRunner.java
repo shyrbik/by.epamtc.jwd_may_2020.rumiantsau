7287a5f407eb5dedc62273a9a87566e5b25ad887
Borodachevi <Figvam_123>
2020-02-09 22:26:52 +0300

    Calc SaveLoadFiles

--- src/by/it/borodachev/Calc/ConsoleRunner.java
+++ src/by/it/borodachev/Calc/ConsoleRunner.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class ConsoleRunner {
 
     public static void main(String[] args) throws Exception {
+        Var.loadvar();
         Scanner scanner = new Scanner(System.in);
         Parser parser = new Parser();
         Printer printer = new Printer();
         while (true){
             String expression = scanner.next();
             if (expression.equals("end"))
+            {
+                Var.savevar();
                 break;
+            }
             try {
                 Var result = parser.calc(expression);
                 printer.print(result);
