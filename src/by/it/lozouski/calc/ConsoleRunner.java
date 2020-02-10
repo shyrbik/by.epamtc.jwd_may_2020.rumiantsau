030322a668e9b5acb4e2e4ffb429de35e600984c
Andygo298 <andygo298@gmail.com>
2020-02-11 00:04:16 +0300

    calc уже почти:)

--- src/by/it/lozouski/calc/ConsoleRunner.java
+++ src/by/it/lozouski/calc/ConsoleRunner.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 class ConsoleRunner {
     public static void main(String[] args) {
+        Logging.logFileRecord("The program was run.");
         Scanner sc1 = new Scanner(System.in);
         String inputLine;
         Parser parser = new Parser();
@@ -17,11 +18,17 @@ class ConsoleRunner {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        while (!(inputLine = sc1.nextLine()).equals("end")) {
+
+        while (true) {
+            inputLine = sc1.nextLine();
+            if (inputLine.equals("end")){
+                Logging.logFileRecord("The program was end."+'\n'
+                                                      +"***************************************************************************");
+                break;
+            }
             try {
                 Logging.logFileRecord(inputLine);
                 Var result = parser.calculate(inputLine);
-                Logging.logFileRecord(inputLine);
                 printer.print(result);
                 Logging.logFileRecord("Result: " + result.toString());
             } catch (CalcException e) {
