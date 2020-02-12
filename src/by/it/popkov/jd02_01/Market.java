78886655f11e49804dc3d24ea52a17126417cc2a
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-12 18:16:23 +0300

    TaskC

--- src/by/it/popkov/jd02_01/Market.java
+++ src/by/it/popkov/jd02_01/Market.java
@@ -11,16 +11,11 @@ class Market {
         int counter = 1;
         int letIn = 0;
         for (int t = 0; t < 120; t++) {
-//            System.out.println("Sec = " + t + "Count = " + Dispatcher.buyerCounter);
-            if (Dispatcher.buyerCounter < t + 10 && t < 30) { //UP
-                letIn = 10;
-            }else if (Dispatcher.buyerCounter < t - 50 && (t > 60 && t < 90)) { //UP
-                letIn = 10;
-            }else if (Dispatcher.buyerCounter <= 40 + (30 - t) && (t >= 30 && t <= 60)) { //DOWN
-                letIn = 10;
-            }else if (Dispatcher.buyerCounter <= 40 + (90 - t) && (t >= 90)) { //DOWN
-                letIn = 10;
-            } else continue;
+            if (t == 0) letIn = 10;
+            if (t > 0 && t < 30) letIn = (t + 10) / 6; //UP
+            if (t >= 30 && t < 60) letIn = (40 + (30 - t)) / 6; //DOWN
+            if (t >= 60 && t < 90) letIn = ((t - 60) + 10) / 6; //UP
+            if (t >= 90) letIn = (40 + (90 - t)) / 6; //DOWN
 //           letIn = Helper.randNumUntil(2);
             for (int j = 1; j <= letIn; j++) {
                 Buyer buyer = new Buyer(counter++);
@@ -32,6 +27,7 @@ class Market {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+//            System.out.println("Sec = " + t + "Count = " + Dispatcher.buyerCounter);
         }
         for (Buyer buyer : buyerList) {
             try {
@@ -53,15 +49,12 @@ class Market {
 //        if (t >= 90) letIn = (40 + (90 - t))/6; //DOWN
 
 
-// if (Dispatcher.buyerCounter < t + 10 && t < 30) { //UP
-//        letIn = 6;
-//        }
-//        if (Dispatcher.buyerCounter < t-50 && (t > 60 && t < 90)){
-//        letIn = 6;
-//        } //UP
-//        if (Dispatcher.buyerCounter <= 40 + (30-t)){
-//        letIn = 6;
-//        } //DOWN
-//        if (Dispatcher.buyerCounter <= 40 + (90-t)){
-//        letIn = 6;
-//        }else continue;
+//if (Dispatcher.buyerCounter < t + 10 && t < 30) { //UP
+////        letIn = 10;
+////        }else if (Dispatcher.buyerCounter < t - 50 && (t > 60 && t < 90)) { //UP
+////        letIn = 10;
+////        }else if (Dispatcher.buyerCounter <= 40 + (30 - t) && (t >= 30 && t <= 60)) { //DOWN
+////        letIn = 10;
+////        }else if (Dispatcher.buyerCounter <= 40 + (90 - t) && (t >= 90)) { //DOWN
+////        letIn = 10;
+////        } else continue;
