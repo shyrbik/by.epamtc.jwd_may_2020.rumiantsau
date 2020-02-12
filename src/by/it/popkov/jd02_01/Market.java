fd3413e46ce5376fc76d87ed1b33ebb13cd37064
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-12 18:07:36 +0300

    TaskC is half

--- src/by/it/popkov/jd02_01/Market.java
+++ src/by/it/popkov/jd02_01/Market.java
@@ -11,8 +11,18 @@ class Market {
         int counter = 1;
         int letIn = 0;
         for (int t = 0; t < 120; t++) {
-           letIn = Helper.randNumUntil(2);
-           for (int j = 1; j <= letIn; j++) {
+//            System.out.println("Sec = " + t + "Count = " + Dispatcher.buyerCounter);
+            if (Dispatcher.buyerCounter < t + 10 && t < 30) { //UP
+                letIn = 10;
+            }else if (Dispatcher.buyerCounter < t - 50 && (t > 60 && t < 90)) { //UP
+                letIn = 10;
+            }else if (Dispatcher.buyerCounter <= 40 + (30 - t) && (t >= 30 && t <= 60)) { //DOWN
+                letIn = 10;
+            }else if (Dispatcher.buyerCounter <= 40 + (90 - t) && (t >= 90)) { //DOWN
+                letIn = 10;
+            } else continue;
+//           letIn = Helper.randNumUntil(2);
+            for (int j = 1; j <= letIn; j++) {
                 Buyer buyer = new Buyer(counter++);
                 buyer.start();
                 buyerList.add(buyer);
@@ -22,7 +32,6 @@ class Market {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-//            System.out.println("Sec = " + t + "Count = " + Dispatcher.buyerCounter);
         }
         for (Buyer buyer : buyerList) {
             try {
