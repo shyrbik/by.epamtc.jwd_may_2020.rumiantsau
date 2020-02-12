ac7322807b9c9989e18b7e80dda86e378fc82d07
Borodachevi <Figvam_123>
2020-02-12 22:57:27 +0300

    JD 02_01

---
+++ src/by/it/borodachev/jd02_01/Buyer.java
@@ -0,0 +1,41 @@
+package by.it.borodachev.jd02_01;
+
+class Buyer extends Thread implements IBuyer{
+
+    public Buyer(int number) {
+        super("Buyer №"+number+" ");
+        Dispatcher.countBuyer++;
+    }
+
+    @Override
+    public void run() {
+        enterToMarket();
+        chooseGoods();
+        goOut();
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this+"enter to the market");
+    }
+
+    @Override
+    public void chooseGoods() {
+        System.out.println(this+"started choosing by goods");
+        int timeout= Helper.random(500,2000);
+        Helper.sleep(timeout);
+        System.out.println(this+"finshed choosing by goods");
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this+"go from the market");
+        Dispatcher.countBuyer--;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
---
+++ src/by/it/borodachev/jd02_01/Dispatcher.java
@@ -0,0 +1,16 @@
+package by.it.borodachev.jd02_01;
+
+import java.util.HashMap;
+
+class Dispatcher {
+
+    static final int K_SPEED=10000;
+    static int countBuyer=0;
+    static  HashMap<String,Double> goods=new HashMap<>();
+    static {
+        for (int i = 0; i <100; i++) {
+            goods.put("good "+i, Helper.randomPrice(100)) ;
+        }
+    }
+
+}
