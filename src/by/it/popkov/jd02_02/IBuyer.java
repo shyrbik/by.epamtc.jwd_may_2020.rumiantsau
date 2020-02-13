8e133562e72b28aa3b2fde6f9f272655c2396554
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-13 17:11:22 +0300

    Add

---
+++ src/by/it/popkov/jd02_02/IBuyer.java
@@ -0,0 +1,7 @@
+package by.it.popkov.jd02_02;
+
+interface IBuyer {
+    void enterToMarket(); //вошел в магазин (мгновенно)
+    void chooseGoods(); //выбрал товар (от 0,5 до 2 секунд)
+    void goOut(); //отправился на выход(мгновенно)
+}
