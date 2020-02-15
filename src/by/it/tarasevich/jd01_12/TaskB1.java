db12964387d7219b34656035623573c9ee97c2a1
tarasevich <yuliya6149116@gmail.com>
2020-02-15 22:18:44 +0300

    Task15 A Complete

--- src/by/it/tarasevich/jd01_12/TaskB1.java
+++ src/by/it/tarasevich/jd01_12/TaskB1.java
@@ -6,15 +6,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class TaskB1 {
     public static void main(String[] args) {
-
-        Map<String, Integer> num = new HashMap<>();
-        Scanner scanner = new Scanner(System.in);
-        String str;
-        while (!(str = scanner.nextLine()).equals("end")) {
-
-
+        Map<String, Integer> map = new HashMap<>();
+        Scanner scanner = new Scanner(System.in); //считываем текст
+        while (true) {
+            String str = scanner.nextLine();
+            String[] line = str.split("[^A-Za-z']+");
+            if (str.equals("end")) break;
+            for (String word : line) {
+                if (map.containsKey(word)) {
+                    Integer integer = map.get(word);
+                    integer++;
+                    map.put(word, integer);
+                } else {
+                    map.put(word, 1);
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> values : map.entrySet()) {
+            System.out.println(values.getKey() + "=" + values.getValue());
         }
     }
 }
