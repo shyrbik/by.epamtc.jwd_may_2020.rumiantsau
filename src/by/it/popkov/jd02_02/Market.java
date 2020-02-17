a7406675e74c70230ca73e1a728e21fb79be5e31
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-17 10:49:54 +0300

    TaskC

--- src/by/it/popkov/jd02_02/Market.java
+++ src/by/it/popkov/jd02_02/Market.java
@@ -8,12 +8,13 @@ class Market {
     public static void main(String[] args) {
         Helper.writeGoodsMap();
         System.out.println("****** Open shop ******");
-        List<Thread> manInMarket = new ArrayList<>(6000);
+        List<Buyer> buyersInMarket = new ArrayList<>(1000);
+        List<Thread> cashierInMarket = new ArrayList<>(Dispatcher.CASHIER_MAX);
 
         for (int i = 1; i <= Dispatcher.CASHIER_MAX; i++) {
             Cashier cashier = new Cashier(i);
             Thread thread = new Thread(cashier);
-            manInMarket.add(thread);
+            cashierInMarket.add(thread);
             thread.start();
         }
 
@@ -28,19 +29,28 @@ class Market {
                 if (Dispatcher.planIsNotCompleted()) {
                     Buyer buyer = new Buyer(counter++);
                     buyer.start();
-                    manInMarket.add(buyer);
+                    buyersInMarket.add(buyer);
                 }
             }
             Helper.delay(1000);
         }
 
-        for (Thread man : manInMarket) {
+        for (Buyer buyer : buyersInMarket) {
             try {
-                man.join();
+                buyer.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        for (Thread cashier : cashierInMarket) {
+            try {
+                cashier.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
         System.out.println("****** Close shop ******");
 
     }
