69f4b72817af953513176e9cd8f56729deffab7e
sjubusel <sju.busel@gmail.com>
2020-02-17 17:18:36 +0200

    Busiel Siarhei -  JD02_02 - add Cashier class

---
+++ src/by/it/busel/jd02_02/Cashier.java
@@ -0,0 +1,4 @@
+package by.it.busel.jd02_02;
+
+public class Cashier extends Thread {
+}
--- src/by/it/busel/jd02_02/Dispatcher.java
+++ src/by/it/busel/jd02_02/Dispatcher.java
@@ -2,7 +2,7 @@ package by.it.busel.jd02_02;
 
 class Dispatcher {
     static final int SPEED_COEFFICIENT = 100;
-
+    //todo maybe replace with  AtomicInteger
     private static int counterOfBuyersInside = 0;
 
     static synchronized int get() {
