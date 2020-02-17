a5baf5a6b9ddbab12d3cb440ae4146547a7866a8
sjubusel <sju.busel@gmail.com>
2020-02-17 17:44:07 +0200

    Busiel Siarhei -  JD02_02 - add SoleQueue

--- src/by/it/busel/jd02_02/Cashier.java
+++ src/by/it/busel/jd02_02/Cashier.java
@@ -1,11 +1,14 @@
 package by.it.busel.jd02_02;
 
-public class Cashier extends Thread {
+class Cashier extends Thread {
     private String name;
 
     Cashier(int id) {
         this.name = "Cashier №" + id + " ";
     }
 
+    @Override
+    public void run() {
 
+    }
 }
