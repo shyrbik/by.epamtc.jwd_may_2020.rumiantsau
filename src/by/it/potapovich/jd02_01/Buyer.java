300610c127bee5301e1318ffaac12b9c46ed2d89
kristina potapovich <ryzhik1191@gmail.com>
2020-02-17 14:43:22 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2019-12-03

--- src/by/it/potapovich/jd02_01/Buyer.java
+++ src/by/it/potapovich/jd02_01/Buyer.java
@@ -1,17 +1,39 @@
 package by.it.potapovich.jd02_01;
 
-public class Buyer extends Thread implements Runnable, IBuyer{
-    int num;
-    public Buyer (int num) {
+import java.util.*;
+
+public class Buyer extends Thread implements Runnable, IBuyer, IUseBacket {
+    private int num;
+    private String name;
+    private Backet backet;
+    private Good good;
+    private Map<Good, Integer> goods;
+    private Set<Good> backets;
+
+
+
+    Buyer(int num, Map<Good, Integer> goods, Backet backet) throws InterruptedException {
         this.num = num;
         this.setName("Покупатель № " + num + " ");
+        this.goods = goods;
+        this.backet = backet;
         start();
+    }
+
+    public Map<Good, Integer> getGoods() {
+        return goods;
+    }
+
+    public void setGoods(Map<Good, Integer> goods) {
+        this.goods = goods;
     }
 
     @Override
     public void run() {
         enterToMarket();
+        takeBacket();
         chooseGoods();
+        putGoodsToBacket();
         goOut();
     }
 
@@ -27,20 +49,70 @@ public class Buyer extends Thread implements Runnable, IBuyer{
     }
 
     @Override
+    public void takeBacket() {
+        //  if (name != null) {
+        backet = new Backet();
+       chooseGoods();
+
+
+        System.out.println(this + "взял корзину");
+
+
+        //    }
+    }
+
+    @Override
     public void chooseGoods() {
         try {
-            int pause = Rnd.fromTo(500,2000);
+            int pause = Helper.random(500, 2000);
             Thread.sleep(pause);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             System.err.println(this + "выбрал товар");
         }
 
     }
 
+
+
     @Override
+    public void putGoodsToBacket() {
+        // if (goods != null){
+        int count = (int) (Math.random() * 4) + 1;
+        for (int i = 0; i <= count; i++) {
+            int index = (int) (Math.random() * 9);
+            // Random random = new Random();
+           Good [] goodsArray = goods.keySet().toArray(new Good[0]);
+
+           good = goodsArray[index];
+           if (backets == null){
+               backets = new HashSet<>();
+           }
+            if (good != null){
+                backets.add(good);
+                chooseGoods();
+                System.out.println("Покупатель № " + this + "положил " + backets + "в корзину");
+            }
+
+        }
+
+
+            }
+
+
+
+
+        // backet.setBackets();
+
+        // goods = new LinkedHashMap<>();
+        //System.out.println(this + "положил товар в корзину");
+
+
+        //  }
+
+
+     @Override
     public void goOut() {
         System.out.println(this + "вышел из магазина");
-
 
     }
 }
