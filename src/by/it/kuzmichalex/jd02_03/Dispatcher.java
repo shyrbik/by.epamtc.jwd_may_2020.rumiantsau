6b94d659d48310aaa27700b2aca3a00b92f0b10e
kuzmichalex <58950628+kuzmichalex@users.noreply.github.com>
2020-02-18 20:32:24 +0300

    Make import great again!

---
+++ src/by/it/kuzmichalex/jd02_03/Dispatcher.java
@@ -0,0 +1,78 @@
+package by.it.kuzmichalex.jd02_03;
+
+class Dispatcher {
+    static final int MAX_CASHIERS = 5;
+    static final int PLAN = 100;
+    static final int MIN_BUYERS = 10;
+    static final int MAX_BUYERS = 35;
+
+    static final Object monitorBuyers = new Object();
+    static volatile int countOfBuyers = 0;
+    static volatile int countOfBuyersDone = 0;
+
+    static final Object monitorTotalAmount = new Object();
+    static volatile double marketTotalAmount = 0.0;
+
+
+
+    /**
+     * Получить количество требуемых покупателей в зависимости от времени.
+     * MIN_BUYERS в начале минуты, MAX_BUYERS в середине минуты, примерно вот так:
+     * ____/ \       /\
+     * __/    \    /   \
+     * /       \/       \
+     * _______________________t
+     *
+     * @param nSecond секунда
+     * @return требуемое количество покупателей
+     */
+    static int getBuyersNeed(int nSecond) {
+        int nPeriod = nSecond % 60;
+        if (nPeriod < 30) return MIN_BUYERS + (MAX_BUYERS - MIN_BUYERS) * nPeriod / 30;    //Восходящий полупериод
+        return MAX_BUYERS - (MAX_BUYERS - MIN_BUYERS) * (nPeriod % 30) / 30;               //Нисходящий полупериод
+    }
+
+    static int getCountBuyersInside() {
+        synchronized (monitorBuyers) {
+            return countOfBuyers;
+        }
+    }
+
+
+
+    static int getCountBuyersDone() {
+        synchronized (monitorBuyers) {
+            return countOfBuyersDone;
+        }
+    }
+
+    static boolean isAllBuyersDone() {
+        synchronized (monitorBuyers) {
+            return countOfBuyersDone == PLAN;
+        }
+    }
+
+    static void enterToMarket() {
+        synchronized (monitorBuyers) {
+            countOfBuyers++;
+        }
+    }
+
+    static void goOut() {
+        synchronized (monitorBuyers) {
+            countOfBuyers--;
+            countOfBuyersDone++;
+        }
+    }
+
+    static void addMarketAmount(double amount){
+        synchronized (monitorTotalAmount){
+            marketTotalAmount+=amount;
+        }
+    }
+
+    static double getMarketTotalAmount(){
+        return marketTotalAmount;
+    }
+
+}
---
+++ src/by/it/kuzmichalex/jd02_03/Market.java
@@ -0,0 +1,47 @@
+package by.it.kuzmichalex.jd02_03;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Market {
+    public static void main(String[] args) {
+
+        Goods goods = new Goods();
+       PseudoGraph graph = new PseudoGraph();
+        CashierManager manager = new CashierManager();
+        BuyerQueue queue = new BuyerQueue(manager);
+        List<Buyer> listBuyers = new ArrayList<>(100);
+
+       // System.out.println("=====MARKET OPENED=====");
+        int nSeconds = 0;
+        int buyersCreated = 0;
+        while (buyersCreated != Dispatcher.PLAN) {
+            int newComers = Dispatcher.getBuyersNeed(nSeconds) - Dispatcher.getCountBuyersInside();
+            if (newComers > 0) {
+                if ((buyersCreated + newComers) > (Dispatcher.PLAN-1)) newComers = ((Dispatcher.PLAN) - buyersCreated);
+                //Запуск берлаг
+                while (newComers > 0) {
+                    Buyer buyer = new Buyer(buyersCreated++);
+                    listBuyers.add(buyer);
+                    buyer.start();
+                    newComers--;
+                }
+            }
+            graph.add(Dispatcher.getCountBuyersInside());
+            TimeHelper.sleep(TimeHelper.getRandom(1000, 1000));
+            nSeconds++;
+        }
+
+    //    System.out.println("====NO ENTER=====");
+        manager.stopManaging();
+
+        System.out.println("======MARKET CLOSED======");
+        System.out.println("Фактор ускорения просмотра TimeHelper.SUPER_SPEED: " + TimeHelper.getSuperSpeed());
+        System.out.println("Фактор замедления пенсионера TimeHelper.SLOW_SPEED: " + TimeHelper.getSlowSpeed());
+        System.out.println("Покупатели по времени: ");
+        System.out.println(graph);
+
+
+    }
+}
