7db24ba462c24705bd4237f194b0c50537528423
henadzi.pluhatar <henadzi.pluhatar@gmail.com>
2020-02-18 04:22:55 +0300

    [+]: Task JD01_15 calc;

--- src/by/it/plugatar/calc/Parser.java
+++ src/by/it/plugatar/calc/Parser.java
@@ -4,30 +4,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static by.it.plugatar.calc.Patterns.OPERATION;
+import static by.it.plugatar.calc.Var.*;
 
 class Parser {
-    Var calc(String expression) throws CalcException{
-        Pattern cal = Pattern.compile(OPERATION);
-        String[] split = expression.trim().split(OPERATION);
-        Var two = Var.createVar(split[1].trim());
-        if (expression.contains("=") ){
-            return Var.saveVar(split[0],two);
-        }
-        Var one = Var.createVar(split[0].trim());
 
-        if (one == null || two == null){
-            //System.out.println("ОПЕРАЦИЯ " + expression + " НЕВОЗМОЖНА");
-            //return null;//TODO CREATE ERROR
-            throw new CalcException("ERROR:" + expression + " ОПЕРАЦИЯ НЕВОЗМОЖНА");
+    Var calc(String expression) throws CalcException{
+        expression = expression.replaceAll("\\s+", "");
+        if (expression.equals("printvar")){
+            printvar();
         }
-        Matcher m = cal.matcher(expression);
-        if (m.find()){
-            String operation = m.group();
-            switch (operation){
-                case "+": return one.add(two);
-                case "-": return one.sub(two);
-                case "*": return one.mul(two);
-                case "/": return one.div(two);
+        else if (expression.equals("sortvar")){
+            sortvar();
+        }
+        else {
+            Pattern cal = Pattern.compile(OPERATION);
+            String[] split = expression.trim().split(OPERATION);
+            Var two = Var.createVar(split[1].trim());
+            if (expression.contains("=")) {
+                return Var.saveVar(split[0], two);
+            }
+            Var one = Var.createVar(split[0].trim());
+
+            if (one == null || two == null) {
+                //System.out.println("ОПЕРАЦИЯ " + expression + " НЕВОЗМОЖНА");
+                //return null;
+                throw new CalcException("ERROR:" + expression + " ОПЕРАЦИЯ НЕВОЗМОЖНА");
+            }
+            Matcher m = cal.matcher(expression);
+            if (m.find()) {
+                String operation = m.group();
+                switch (operation) {
+                    case "+":
+                        return one.add(two);
+                    case "-":
+                        return one.sub(two);
+                    case "*":
+                        return one.mul(two);
+                    case "/":
+                        return one.div(two);
+                }
             }
         }
         return null;
