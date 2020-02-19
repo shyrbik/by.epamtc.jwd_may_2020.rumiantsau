be1ce3b3640d36ac4b1297043259e7ea02625595
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-19 21:39:16 +0300

    TaskC is done

--- src/by/it/popkov/jd02_02/Cashier.java
+++ src/by/it/popkov/jd02_02/Cashier.java
@@ -1,5 +1,7 @@
 package by.it.popkov.jd02_02;
 
+import java.util.Map;
+
 class Cashier implements Runnable {
     private final Object monitorCashier;
 
@@ -51,11 +53,49 @@ class Cashier implements Runnable {
     private void serve(Buyer buyer) {
         System.out.println(this + " start serve " + buyer);
         Helper.delay(Helper.randNum(2000, 5000)); //Time to serve
-        System.out.println(this + " print " + buyer + " check: " + buyer.getBasket() + " SUM: "
-                + buyer.getBasket().values().stream().mapToInt(Integer::intValue).sum());
+        printCheck(buyer);
         System.out.println(this + " finished serve " + buyer);
-        synchronized (buyer) { //Finished serve, buyer can continue
+        synchronized (buyer) {
             buyer.notify();
         }
+    }
+
+    private void printCheck(Buyer buyer) {
+        int sumOfCheck = buyer.getBasket().values().stream().mapToInt(Integer::intValue).sum();
+        dispatcher.plusDayProfit(sumOfCheck);
+        String tab = "";
+        String posBuyerOnline = "";
+        StringBuffer stringBuffer = new StringBuffer();
+        System.out.println(this + " print " + buyer + " check");
+        switch (this.name) {
+            case "Cashier 1":
+                tab = "%10s";
+                posBuyerOnline = "%150s";
+                break;
+            case "Cashier 2":
+                tab = "%40s";
+                posBuyerOnline = "%120s";
+                break;
+            case "Cashier 3":
+                tab = "%70s";
+                posBuyerOnline = "%90s";
+                break;
+            case "Cashier 4":
+                tab = "%100s";
+                posBuyerOnline = "%60s";
+                break;
+            case "Cashier 5":
+                tab = "%130s";
+                posBuyerOnline = "%30s";
+                break;
+        }
+        stringBuffer.append(String.format(tab + "%-30s" + posBuyerOnline + "%20s" +  "\n","", buyer + " check:",
+                "Buyer queue size:" + buyerQueue.getBuyerQueueSize(), "Day profit: " + dispatcher.getDayProfit()));
+        for (Map.Entry<String, Integer> entry : buyer.getBasket().entrySet()) {
+            stringBuffer.append(String.format(tab + "%-30s" + "\n", "", entry.getKey() + " = " + entry.getValue()));
+        }
+        stringBuffer.append(String.format(tab + "%-30s" + "\n", "", "SUM: " + sumOfCheck));
+        stringBuffer.append(String.format(tab + "%-30s" + "\n", "", "Cashier: " + this.name));
+        System.out.println(stringBuffer);
     }
 }
