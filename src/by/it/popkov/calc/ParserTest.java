e031d18e76d178e517bfc7a386ddb730390f756a
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-21 19:08:13 +0300

    TaskA

--- src/by/it/popkov/calc/ParserTest.java
+++ src/by/it/popkov/calc/ParserTest.java
@@ -10,9 +10,12 @@ public class ParserTest {
 
     @Test
     public void a1() {
+        System.out.println("Test A=2+5.3");
         try {
             Var calc = parser.calc("A=2+5.3");
             assertEquals("Wrong result for A=2+5.3", "7.3", calc.toString());
+            System.out.println(calc.toString());
+            System.out.println("Test is successful, so you see this message");
         } catch (CalcException e) {
             e.printStackTrace();
         }
@@ -20,9 +23,12 @@ public class ParserTest {
 
     @Test
     public void a2() {
+        System.out.println("Test B=A*3.5");
         try {
             Var calc = parser.calc("B=A*3.5");
             assertEquals("Wrong result for B=A*3.5", "25.55", calc.toString());
+            System.out.println(calc.toString());
+            System.out.println("Test is successful, so you see this message");
         } catch (CalcException e) {
             e.printStackTrace();
         }
@@ -30,9 +36,12 @@ public class ParserTest {
 
     @Test
     public void a3() {
+        System.out.println("Test B1=B+0.11*-5");
         try {
             Var calc = parser.calc("B1=B+0.11*-5");
             assertEquals("Wrong result for B1=B+0.11*-5", "25.0", calc.toString());
+            System.out.println(calc.toString());
+            System.out.println("Test is successful, so you see this message");
         } catch (CalcException e) {
             e.printStackTrace();
         }
@@ -40,9 +49,12 @@ public class ParserTest {
 
     @Test
     public void a4() {
+        System.out.println("Test B2=A/2-1");
         try {
             Var calc = parser.calc("B2=A/2-1");
             assertEquals("Wrong result for B2=A/2-1", "2.65", calc.toString());
+            System.out.println(calc.toString());
+            System.out.println("Test is successful, so you see this message");
         } catch (CalcException e) {
             e.printStackTrace();
         }
