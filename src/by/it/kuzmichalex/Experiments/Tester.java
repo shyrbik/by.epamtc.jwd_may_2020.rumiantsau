d979740ebd2458a568d1a670027a0b62f221b096
kuzmichalex <58950628+kuzmichalex@users.noreply.github.com>
2020-02-23 01:36:43 +0300

    Calc_jd02_04

--- src/by/it/kuzmichalex/Experiments/Tester.java
+++
@@ -1,9 +0,0 @@
-package by.it.kuzmichalex.Experiments;
-
-public class Tester {
-    public static void main(String[] args) {
-        ReversePolishNotation rpn = new ReversePolishNotation();
-        rpn.parseExpression("1/(2-3)");
-    }
-
-}
