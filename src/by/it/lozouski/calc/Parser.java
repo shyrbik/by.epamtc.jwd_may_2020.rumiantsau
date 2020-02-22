2c4d44f389ae3acef2d0d71160816d1b151ff640
Andygo298 <andygo298@gmail.com>
2020-02-22 17:28:15 +0300

    calc + test -- taskA

--- src/by/it/lozouski/calc/Parser.java
+++ src/by/it/lozouski/calc/Parser.java
@@ -1,35 +1,83 @@
 package by.it.lozouski.calc;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Parser {
-    Var calculate(String expression) throws CalcException {
-        expression = expression.replace("\\s+", "");
-        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
-        if (matcher.find()) {
-            String operation = matcher.group();
-            String[] operands = expression.split(Patterns.OPERATION, 2);
-            Var operandSecond = Var.createVar(operands[1]);
-            if (operation.equals("=")) {
-                Var.saveVar(operands[0], operandSecond);
-                return operandSecond;
-            }
 
-            Var operandFirst = Var.createVar(operands[0]);
-            if (operandFirst != null && operandSecond != null) {
-                switch (operation) {
-                    case "+":
-                        return operandFirst.add(operandSecond);
-                    case "-":
-                        return operandFirst.sub(operandSecond);
-                    case "*":
-                        return operandFirst.mul(operandSecond);
-                    case "/":
-                        return operandFirst.div(operandSecond);
-                }
+    private static final Map<String, Integer> mapOperationPriority = new HashMap<>();
+
+    static {
+        mapOperationPriority.put("=", 0);
+        mapOperationPriority.put("+", 1);
+        mapOperationPriority.put("-", 1);
+        mapOperationPriority.put("*", 2);
+        mapOperationPriority.put("/", 2);
+    }
+
+    private int getIndexOperation(List<String> listOperations) {
+        int indexOperation = -1;
+        int currentPriorityOperation = -1;
+        for (int i = 0; i < listOperations.size(); i++) {
+            String operation = listOperations.get(i);
+            if (mapOperationPriority.get(operation) > currentPriorityOperation) {
+                indexOperation = i;
+                currentPriorityOperation = mapOperationPriority.get(operation);
+            }
+        }
+        return indexOperation;
+    }
+
+    private Var calculateOneOperation(String leftOperand, String operationType, String rightOperand) throws CalcException {
+        Var rightOper = Var.createVar(rightOperand);
+        if (operationType.equals("=")) {
+            Var.saveVar(leftOperand, rightOper);
+            return rightOper;
+        }
+
+        Var leftOper = Var.createVar(leftOperand);
+        if (leftOper != null && rightOper != null) {
+            switch (operationType) {
+                case "+":
+                    return leftOper.add(rightOper);
+                case "-":
+                    return leftOper.sub(rightOper);
+                case "*":
+                    return leftOper.mul(rightOper);
+                case "/":
+                    return leftOper.div(rightOper);
             }
         }
         throw new CalcException("Incorrect variable creation, please try again.");
+    }
+
+
+    Var calculate(String expression) throws CalcException {
+        expression = expression.replaceAll("\\s+", "");
+
+        List<String> operandsList = new ArrayList<>(Arrays.asList(expression.split(Patterns.OPERATION)));
+        List<String> operationsList = new ArrayList<>();
+
+        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
+        while (matcher.find()) {
+            String operation = matcher.group();
+            operationsList.add(operation);
+        }
+
+        while (operationsList.size() > 0) {
+            try {
+                int i = getIndexOperation(operationsList);
+                String operation = operationsList.remove(i);
+                String leftOperand = operandsList.remove(i);
+                String rightOperand = operandsList.remove(i);
+                Var resultOperations = calculateOneOperation(leftOperand, operation, rightOperand);
+                operandsList.add(i, resultOperations.toString());
+            }catch (IndexOutOfBoundsException e){
+                throw new CalcException("Wrong Input! Nothing to calculate. ");
+            }
+
+        }
+        return Var.createVar(operandsList.get(0));
     }
 }
