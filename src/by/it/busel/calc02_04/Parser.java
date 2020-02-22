0f6b672b5966632adc27c1727eab3d1fbd32de8e
sjubusel <sju.busel@gmail.com>
2020-02-22 23:06:43 +0200

    Busiel Siarhei - JD02_04_calc - C complete

--- src/by/it/busel/calc02_04/Parser.java
+++ src/by/it/busel/calc02_04/Parser.java
@@ -36,6 +36,118 @@ class Parser {
     }
 
     /**
+     * a method, which calculates the result of mathematical operations
+     * on the basis of the argument "String varExpression",
+     * which contains mathematical operators and operands
+     * received from console input by the means of a Scanner-object
+     *
+     * @param varExpression a String-object, which contains
+     *                      mathematical operators and operands
+     *                      received from console input by the means
+     *                      of a Scanner-object
+     * @return a result of mathematical operations,
+     * i.e. a Scalar / Vector / Matrix expression
+     * or assigning of a Var expression
+     */
+    Var calc(String varExpression) throws CalcException {
+        Pattern parenthesesPattern = Pattern.compile("\\(([-+*/0-9.,{}]+)\\)");
+        Matcher parenthesesMatcher = parenthesesPattern.matcher(varExpression);
+        while (parenthesesMatcher.find()) {
+            String group = parenthesesMatcher.group();
+            String strResult = group.substring(1, group.length() - 1);
+            Var result = processWithoutParentheses(strResult);
+            varExpression = parenthesesMatcher.replaceAll(result.toString());
+            parenthesesMatcher.reset(varExpression);
+        }
+        return processWithoutParentheses(varExpression);
+    }
+
+    /**
+     * a method that performs mathematical operations
+     *
+     * @param varExpression a String object on which mathematical operations and other operations
+     *                      are performed
+     * @return a result of of mathematical operations
+     * i.e. a Scalar / Vector / Matrix expression
+     * or assigning of a Var expression
+     * @throws CalcException in the following cases:
+     *                       1) if only a mathematical operator is inputted;
+     *                       2) if a wrong Var expression is inputted
+     */
+    private Var processWithoutParentheses(String varExpression) throws CalcException {
+        List<String> operands = new ArrayList<>(Arrays.asList(varExpression.split(Patterns.OPERATION)));
+        if (verifyIfFirstCharIsMinusOrPlus(varExpression)
+                && ifOneOrMoreOperandsExistOtherwiseThrowCalcException(operands)) operands.set(0, "0");
+        List<String> operators = new ArrayList<>();
+        fillOperatorsIfTheyExist(varExpression, operators);
+        performMathOperationsAccordingToTheirPriority(operands, operators);
+        return Var.createVar(operands.get(0));
+    }
+
+    /**
+     * a method that verifies if a first char of an argument "String varExpression"
+     * is a char '-' or '+'
+     *
+     * @param varExpression a String object
+     * @return true if a first char of an argument "String varExpression"
+     * is a char '-' or '+'
+     */
+    private boolean verifyIfFirstCharIsMinusOrPlus(String varExpression) {
+        if (varExpression.length() > 1)
+            return varExpression.charAt(0) == '-' || varExpression.charAt(0) == '+';
+        return false;
+    }
+
+    /**
+     * a method that checks  one of conditions whether a mathematical operation can be performed, i.e.
+     * presence of two mathematical operands
+     *
+     * @param operands a List which should contain operands
+     * @return true if "operands" contains one or more operands
+     * @throws CalcException if three are no operands
+     */
+    private boolean ifOneOrMoreOperandsExistOtherwiseThrowCalcException(List<String> operands) throws CalcException {
+        if (operands.size() < 1) {
+            throw new CalcException("You've entered only a mathematical operator.");
+        }
+        return true;
+    }
+
+    /**
+     * a method that finds mathematical operators and adds them to a List "operators"
+     *
+     * @param varExpression a String that can contain mathematical operators
+     * @param operators     a List to which the found mathematical operators are added
+     */
+    private void fillOperatorsIfTheyExist(String varExpression, List<String> operators) {
+        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(varExpression);
+        while (matcher.find()) {
+            String operator = matcher.group();
+            operators.add(operator);
+        }
+    }
+
+    /**
+     * a method that find mathematical operator with the highest priority
+     * and performs a mathematical operation, repeats this until only one operand is remaining
+     *
+     * @param operands  a mathematical operands
+     * @param operators a mathematical operators
+     * @throws CalcException if a mathematical operation can be performed
+     */
+    private void performMathOperationsAccordingToTheirPriority
+    (List<String> operands, List<String> operators) throws CalcException {
+        while (operators.size() > 0) {
+            int i = getIndex(operators);
+            String operator = operators.remove(i);
+            String left = operands.remove(i);
+            String right = operands.remove(i);
+            Var result = oneSeparateOperation(left, operator, right);
+            operands.add(i, result.toString());
+        }
+    }
+
+    /**
      * a method that returns an index of an operator with the highest priority value
      * from "List<String> operators", which contains unknown operators from a console
      *
@@ -56,131 +168,64 @@ class Parser {
         return index;
     }
 
-    private Var oneOperation(String strLeft, String operator, String strRight) throws CalcException {
+    /**
+     * a method which calculates either one of supported mathematical operations
+     * or assign a value and saves it to "vars.txt"
+     *
+     * @param strLeft  either the first Var expression (scalar) or a name of a Var expression,
+     *                 which is to be assigned
+     * @param operator either an mathematical operator or a "=" operator which is to assign
+     *                 a Var expression
+     * @param strRight a Var expression which is either one to be assigned or the second Var expression
+     * @return a Var expression which has been assigned or a result of a mathematical operation
+     * @throws CalcException if impossible has come true while calculating
+     */
+    private Var oneSeparateOperation(String strLeft, String operator, String strRight) throws CalcException {
         if (operator.equals("=")) {
-            Var right = Var.createVar(strRight.trim());
-            Storage.putVarToStorage(strLeft, right);
-            return right;
+            return assignVar(strLeft, strRight);
         } else {
-            Var right = Var.createVar(strRight.trim());
-            Var left = Var.createVar(strLeft.trim());
-            switch (operator) {
-                case "+":
-                    return left.add(right);
-                case "-":
-                    return left.sub(right);
-                case "*":
-                    return left.mul(right);
-                case "/":
-                    return left.div(right);
-                default:
-                    throw new CalcException("The mathematical operator has not been found.\nCheck the expression You inputted. Then re-enter your expression, please!");
-            }
+            return calculateVar(strLeft, operator, strRight);
         }
     }
 
     /**
-     * a method, which calculates some mathematical operations
-     * on the basis of the argument "String varExpression",
-     * which contains a mathematical operator and its operands
-     * received from console input by the means of a Scanner-object
+     * a method that assigns an inputted Var expression to a name inputted at the same time
      *
-     * @param varExpression a String-object, which contains
-     *                      a mathematical operator and its operands
-     *                      received from console input by the means
-     *                      of a Scanner-object
-     * @return a result of a mathematical operation,
-     * i.e. a Scalar / Vector / Matrix expression
+     * @param strLeft  a name of a var expression
+     * @param strRight a String representation of a Var expression
+     * @return a Var object that containing its value
+     * @throws CalcException if an incorrect Var expression is inputted
      */
-    Var calc(String varExpression) throws CalcException {
-        List<String> operands = new ArrayList<>(Arrays.asList(varExpression.split(Patterns.OPERATION)));
-        if (operands.size() < 1) {
-            throw new CalcException("You've entered only a mathematical operator or only one operand!");
-        } else if (Storage.containsKey(varExpression)) {
-            return Storage.getVarFromStorage(varExpression);
-        }
-        List<String> operators = new ArrayList<>();
-        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(varExpression);
-        while (matcher.find()) {
-            String operator = matcher.group();
-            operators.add(operator);
-        }
-        while (operators.size() > 0) {
-            int i = getIndex(operators);
-            String operator = operators.remove(i);
-            String left = operands.remove(i);
-            String right = operands.remove(i);
-            Var result = oneOperation(left, operator, right);
-            operands.add(i, result.toString());
-        }
-        return Var.createVar(operands.get(0));
-
-        /*String[] operand = varExpression.split(Patterns.OPERATION);
-        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(varExpression);
-        if (matcher.find() && throwCalcExceptionIfOneOrNoOperand(operand)) {
-            String operator = matcher.group();
-            if (operator.equals("=")) {
-                return assignVar(operand);
-            } else {
-                return calculateVar(operand, operator);
-            }
-        } else if (Storage.containsKey(varExpression)) {
-            return Storage.getVarFromStorage(varExpression);
-        }*/
-//        throw new CalcException("The mathematical operator has not been found.\nCheck the expression You inputted. Then re-enter your expression, please!");
-    }
-
-    /**
-     * a method that checks  one of conditions whether a mathematical operation can be performed, i.e.
-     * presence of two mathematical operands
-     *
-     * @param operand an array that should contain two mathematical operands
-     * @return a boolean expression which states that there is two operands
-     * @throws CalcException if a aforementioned condition isn't fulfilled
-     */
-    private boolean throwCalcExceptionIfOneOrNoOperand(String[] operand) throws CalcException {
-        if (operand.length <= 1) {
-            throw new CalcException("You've entered only a mathematical operator or only one operand!");
-        }
-        return true;
+    private Var assignVar(String strLeft, String strRight) throws CalcException {
+        Var right = Var.createVar(strRight);
+        Storage.putVarToStorage(strLeft, right);
+        return right;
     }
 
     /**
      * a method that returns a result of calculation of two Var expressions
      *
-     * @param operand  a String array which contains two mathematical operands
-     * @param operator a operator that determines which mathematical operation will be performed
+     * @param strLeft  the first mathematical operand
+     * @param operator a mathematical operator that determines
+     *                 which mathematical operation will be performed
+     * @param strRight the second mathematical operand
      * @return a result of calculation
-     * @throws CalcException when a mathmetical operation can be performed
+     * @throws CalcException if a mathematical operation can not be performed
      */
-    private Var calculateVar(String[] operand, String operator) throws CalcException {
-        Var operand2 = Var.createVar(operand[1].trim());
-        Var operand1 = Var.createVar(operand[0].trim());
+    private Var calculateVar(String strLeft, String operator, String strRight) throws CalcException {
+        Var right = Var.createVar(strRight);
+        Var left = Var.createVar(strLeft);
         switch (operator) {
             case "+":
-                return operand1.add(operand2);
+                return left.add(right);
             case "-":
-                return operand1.sub(operand2);
+                return left.sub(right);
             case "*":
-                return operand1.mul(operand2);
+                return left.mul(right);
             case "/":
-                return operand1.div(operand2);
+                return left.div(right);
             default:
                 throw new CalcException("The mathematical operator has not been found.\nCheck the expression You inputted. Then re-enter your expression, please!");
         }
-    }
-
-    /**
-     * a method that assigns an inputted Var expression to an inputted at the same time name.
-     *
-     * @param operand an array of two Strings, first one contains a name of a var expression,
-     *                second one - a String representation of a Var expression
-     * @return a Var object that containing its value
-     * @throws CalcException if an incorrect Var expression is inputted
-     */
-    private Var assignVar(String[] operand) throws CalcException {
-        Var operandToAssign = Var.createVar(operand[1].trim());
-        Storage.putVarToStorage(operand[0], operandToAssign);
-        return operandToAssign;
     }
 }
