c4bd01083919ea4edafb5939a21642ca02996c9b
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-25 17:21:24 +0300

    TaskA

--- src/by/it/popkov/jd02_05/Runner.java
+++ src/by/it/popkov/jd02_05/Runner.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 class Runner {
     public static void main(String[] args) {
-        Locale locale = new Locale("en", "US");
+        Locale locale = new Locale("en");
         ResourceBundle rb = ResourceBundle.getBundle("by.it.popkov.jd02_05.resource.hello", locale);
         System.out.println(rb.getString("user.firstName"));
     }
