cbb2002bcb6a89323afc606f27897bb049e74936
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-26 11:20:47 +0300

    TaskB

--- src/by/it/popkov/jd02_05/Runner.java
+++ src/by/it/popkov/jd02_05/Runner.java
@@ -1,17 +1,44 @@
 package by.it.popkov.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 class Runner {
     public static void main(String[] args) {
-        LangSwitcher langSwitcher = LangSwitcher.LANG_SWITCHER;
-        if (args.length==2){
-            Locale locale = new Locale(args[0], args[1]);
-            langSwitcher.setResourceBundle(locale);
-        }
-        System.out.println(langSwitcher.getResourceBundle().getString(Message.WELCOME));
-        System.out.println(langSwitcher.getResourceBundle().getString(User.FIRST_NAME));
-        System.out.println(langSwitcher.getResourceBundle().getString(User.SECOND_NAME));
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            LangSwitcher langSwitch = LangSwitcher.LANG_SWITCHER;
+//            if (args.length==2){
+//                Locale locale = new Locale(args[0], args[1]);
+//                langSwitch.setResourceBundle(locale);
+//            }
+
+            switch (scanner.next()) {
+                case "ru":
+                    langSwitch.setResourceBundle(new Locale("ru", "RU"));
+                    break;
+                case "be":
+                    langSwitch.setResourceBundle(new Locale("be", "BY"));
+                    break;
+                case "en":
+                    langSwitch.setResourceBundle(new Locale("en", "US"));
+                    break;
+            }
+            print(langSwitch);
+
+        }
+
+
+    }
+
+    static void print(LangSwitcher langSwitch) {
+        System.out.println(langSwitch.getResourceBundle().getString(Message.WELCOME));
+        System.out.println(langSwitch.getResourceBundle().getString(User.FIRST_NAME));
+        System.out.println(langSwitch.getResourceBundle().getString(User.SECOND_NAME));
+        DateFormat date = DateFormat.getDateInstance(DateFormat.FULL, langSwitch.getResourceBundle().getLocale());
+        System.out.println(date.format(new Date()));
     }
 }
