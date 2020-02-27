36db6969a73478df35939eb14ee976c4d61fac12
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-27 12:38:27 +0300

    TaskC

--- src/by/it/popkov/calc/Error.java
+++ src/by/it/popkov/calc/Error.java
@@ -1,12 +1,12 @@
 package by.it.popkov.calc;
 
 interface Error {
-    String UNCORRECT_MATRIX_FORMAT = "message.welcome";
-    String UNCORRECT_VECTOR_FORMAT = "message.welcome";
-    String DIVISION_BY_ZERO = "message.welcome";
-    String INPUT_ERROR = "message.welcome";
-    String NULL_RESULT = "message.welcome";
-    String IMPOSSIBLE = "message.welcome";
+    String UNCORRECT_MATRIX_FORMAT = "error.UNCORRECT_MATRIX_FORMAT";
+    String UNCORRECT_VECTOR_FORMAT = "error.UNCORRECT_VECTOR_FORMAT";
+    String DIVISION_BY_ZERO = "error.DIVISION_BY_ZERO";
+    String INPUT_ERROR = "error.INPUT_ERROR";
+    String NULL_RESULT = "error.NULL_RESULT";
+    String IMPOSSIBLE_OPERATION = "error.IMPOSSIBLE";
 
 
 
--- src/by/it/popkov/calc/Var.java
+++ src/by/it/popkov/calc/Var.java
@@ -11,77 +11,77 @@ abstract class Var implements Operation {
     
     @Override
     public Var add(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     Var preAdd(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var add(Scalar other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var add(Vector other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var add(Matrix other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     @Override
     public Var sub(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     Var preSub(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var sub(Scalar other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var sub(Vector other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var sub(Matrix other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     @Override
     public Var mul(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
     Var preMul(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var mul(Scalar other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var mul(Vector other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var mul(Matrix other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     @Override
     public Var div(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     Var preDiv(Var other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
     public Var div(Scalar other) throws CalcException {
-        throw new CalcException(Error.IMPOSSIBLE);
+        throw new CalcException(Error.IMPOSSIBLE_OPERATION);
     }
 
 
