1957ce75697602cecb5037ea23434d54163ec807
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-28 15:19:26 +0300

    TaskC

--- src/by/it/popkov/jd02_05/Convertor.java
+++ src/by/it/popkov/jd02_05/Convertor.java
@@ -1,4 +1,20 @@
 package by.it.popkov.jd02_05;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Convertor {
+    public static void main(String[] args) {
+        try {
+            List<String> stringList = Files.lines(
+                    Paths.get("D:/Prog/IdeaProjects/JD2019-12-04/src/by/it/popkov/jd02_05/txt/hello.txt"))
+                    .collect(Collectors.toList());
+            System.out.println(stringList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
