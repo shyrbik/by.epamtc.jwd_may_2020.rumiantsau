2cd0462ece0d4dd68c56625803fb2d7b873aff9e
sjubusel <sju.busel@gmail.com>
2020-02-28 08:16:57 +0300

    Busiel Siarhei - JD02_05 - A complete

--- src/by/it/busel/jd02_05/InternationalizationTaskA.java
+++ src/by/it/busel/jd02_05/InternationalizationTaskA.java
@@ -1,7 +1,34 @@
 package by.it.busel.jd02_05;
 
-public class InternationalizationTaskA {
-    public static void main(String[] args) {
+import java.util.Locale;
 
+class InternationalizationTaskA {
+    public static void main(String[] args) {
+        ResourcesManager rManager = ResourcesManager.INITIALIZE;
+        verifyIfToChangeLocale(args, rManager);
+        greetUser(rManager);
+        askWaitWish(rManager);
+    }
+
+    private static void verifyIfToChangeLocale(String[] args, ResourcesManager rManager) {
+        if (args.length == 2) {
+            rManager.setLocale(new Locale(args[0], args[1]));
+        }
+    }
+
+    private static void greetUser(ResourcesManager rManager) {
+        System.out.printf("%s, %s %s %s!\n",
+                rManager.get(Message.GREETING), rManager.get(User.FIRST_NAME),
+                rManager.get(User.PATRONYMIC), rManager.get(User.LAST_NAME));
+    }
+
+    private static void askWaitWish(ResourcesManager rManager) {
+        System.out.printf("%s\n", rManager.get(Message.GREETING_QUESTION));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("%s\n", rManager.get(Message.WISHES));
     }
 }
---
+++ src/by/it/busel/jd02_05/Message.java
@@ -0,0 +1,7 @@
+package by.it.busel.jd02_05;
+
+public interface Message {
+    String GREETING = "message.greeting";
+    String GREETING_QUESTION = "message.greetingQuestion";
+    String WISHES = "message.wishes";
+}
