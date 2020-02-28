721eea57a5a9fe172766e7efb38c0c0fe345945c
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2020-02-28 15:24:54 +0300

    TaskC

--- src/by/it/popkov/jd02_05/Converter.java
+++ src/by/it/popkov/jd02_05/Converter.java
@@ -6,7 +6,8 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class Convertor {
+
+class Converter {
     public static void main(String[] args) {
         try {
             List<String> stringList = Files.lines(
