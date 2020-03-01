9c75fb8212c9182334a64e49c45ff30f474ebe10
kristina potapovich <ryzhik1191@gmail.com>
2020-03-01 19:14:59 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2019-12-03

--- src/by/it/potapovich/jd02_05/ResMan.java
+++ src/by/it/potapovich/jd02_05/ResMan.java
@@ -3,20 +3,31 @@ package by.it.potapovich.jd02_05;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-enum ResMan {
+public enum ResMan {
     INSTANCE;
-    private String locationRes = "by.it.potapovich.jd02_05.properties.messages";
+
+    private String locationRes = "by.it.potapovich.jd02_05.messages";
+
     private ResourceBundle resourceBundle;
 
+
     ResMan() {
-        setLocale(Locale.ENGLISH);
+        setLocale(new Locale("en","US"));
+
     }
+    public Locale getLocale(){
+        return resourceBundle.getLocale();
+    }
+
 
     void setLocale(Locale locale) {
         resourceBundle = ResourceBundle.getBundle(locationRes, locale);
     }
 
+
     String get(String key) {
         return resourceBundle.getString(key);
     }
+
+
 }
