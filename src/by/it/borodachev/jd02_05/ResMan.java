0280812d75bd35dacc0d8f5a2b47d71c834e1055
Borodachevi <Figvam_123>
2020-03-02 17:05:41 +0300

    JD02 04 05 All

---
+++ src/by/it/borodachev/jd02_05/ResMan.java
@@ -0,0 +1,20 @@
+package by.it.borodachev.jd02_05;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+enum ResMan {
+    INSTANCE;
+    private String locationRes = "by.it.borodachev.jd02_05.properties.messages";
+    private ResourceBundle resourceBundle;
+    ResMan() {
+        setLocale(Locale.ENGLISH);
+    }
+    void setLocale(Locale locale) {
+        resourceBundle = ResourceBundle.getBundle(locationRes, locale);
+    }
+    String get(String key) {
+        return resourceBundle.getString(key);
+    }
+
+}
