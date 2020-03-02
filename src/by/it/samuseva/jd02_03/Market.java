5ed103f8b89c38e5712e891ab94040ab188fb69b
scokpius <scokpius@gmail.com>
2020-03-02 20:33:11 +0300

    jd02_03 A, B

--- src/by/it/samuseva/jd02_03/Market.java
+++ src/by/it/samuseva/jd02_03/Market.java
@@ -1,8 +1,5 @@
 package by.it.samuseva.jd02_03;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
