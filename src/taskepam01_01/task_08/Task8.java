2f3729a46643e9617a3c73c01907b899d4ca7682
shyrbik <58969166+shyrbik@users.noreply.github.com>
2020-07-03 09:57:32 +0200

    Update Task8.java

--- src/taskepam01_01/task_08/Task8.java
+++ src/taskepam01_01/task_08/Task8.java
@@ -1,4 +1,4 @@
-package taskepam01_01.task_08;
+package by.epamtc.rumiantsau.taskepam01_01.task_08;
 
 public class Task8 {
     public static void main(String[] args) {
@@ -8,15 +8,18 @@ public class Task8 {
         int c = 12;
         int x = a;
         int y = 0;
+        
         while (x <= b){
-       if (x == 15) y= (x + c) *2;
-       else y = (x - c) + 6;
-            System.out.println("y = " + y + " / x = " + x);
+       if (x == 15){
+           y= (x + c) *2;
+       }
+       else {
+           y = (x - c) + 6;
+       }
+        
+        System.out.println("y = " + y + " / x = " + x);
         x = x + h;
         }
-
-
-
 
     }
 }
