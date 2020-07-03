4d0654f68f432d2ca99c74c78fd0a44540fd3f4f
shyrbik <58969166+shyrbik@users.noreply.github.com>
2020-07-03 10:02:00 +0200

    Update Task10.java

--- src/taskepam01_01/task_10/Task10.java
+++ src/taskepam01_01/task_10/Task10.java
@@ -1,5 +1,4 @@
-package taskepam01_01.task_10;
-
+package by.epamtc.rumiantsau.taskepam01_01.task_10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,23 +15,30 @@ public class Task10 {
 
         System.out.println("Input number");
         numberFirst=Integer.parseInt(input.readLine());
-        if (numberFirst != 0)
+        if (numberFirst != 0){
         while((numberSecond= Integer.parseInt(input.readLine())) != 0){
-            if (numberFirst == numberSecond) samePairNumbers = true;
-            if (numberFirst >= numberSecond) increasing = false;
-            if ((numberFirst * numberSecond) > 0) plusMinus = false;
+            if (numberFirst == numberSecond){
+                samePairNumbers = true;
+            }
+            if (numberFirst >= numberSecond){
+                increasing = false;
+            }
+            if ((numberFirst * numberSecond) > 0){
+                plusMinus = false;
+            }
 
             numberFirst = numberSecond;
             countNumbers++;
         }
+        }
 
-        if (countNumbers == 1)
+        if (countNumbers == 1){
             System.out.println("Less than two numbers entered");
-        else
+        }
+        else {
             System.out.println("Is the sequence increasing? " + increasing + "\n"
                     + "Is there at least one pair of identical adjacent numbers in the sequence? " + samePairNumbers
                     + "\n" + "Is the sequence alternating? " + plusMinus);
-
-
+        }
     }
 }
